b88bdfea7c71adb4ba096e31b8a0796e1720e8ed
cwiel <cwiel@224a875b-6a50-0410-9993-82261b5d0d45>
2002-11-01 18:09:35 +0000

    Changed opening #'s to --'s so the file can be executed in psql.

--- schema/trunk/chado/modules/companalysis/companalysis.sql
+++ schema/trunk/chado/modules/companalysis/companalysis.sql
@@ -1,30 +1,30 @@
-## $Id: companalysis.sql,v 1.3 2002-10-31 23:07:03 cmungall Exp $
+--# $Id: companalysis.sql,v 1.4 2002-11-01 18:09:35 cwiel Exp $
 
-# an analysis is a particular execution of a computational analysis;
-# it may be a blast of one sequence against another, or an all by all
-# blast, or a different kind of analysis altogether.
-# it is a single unit of computation - if different blast runs
-# were instantiated over differnet query sequences, there would
-# be multiple entries here.
-#
-# the sequence that was used as the query sequence can be
-# optionally included via queryfeature_id - even though this
-# is redundant with the hitpair table below. this can still
-# be useful - for instance, we may have an analysis that blasts
-# contigs against a database. we may then transform those hits
-# into global coordinates; it can still be useful to keep a record
-# of which contig was blasted as the query.
-#
-# name: a way of grouping analyses. this should be a handy
-# short identifier that can help people find an analysis they
-# want. for instance "tRNAscan", "cDNA", "FlyPep", "SwissProt"
-#
-# program: e.g. blastx, blastp, sim4, genscan
-# programversion: e.g. TBLASTX 2.0MP-WashU [09-Nov-2000]
-# algorithm: eg blast
-# sourcename: eg cDNA, SwissProt
-#
-# MAPPING (bioperl): maps to Bio::Search::Result::ResultI
+-- an analysis is a particular execution of a computational analysis;
+-- it may be a blast of one sequence against another, or an all by all
+-- blast, or a different kind of analysis altogether.
+-- it is a single unit of computation - if different blast runs
+-- were instantiated over differnet query sequences, there would
+-- be multiple entries here.
+--
+-- the sequence that was used as the query sequence can be
+-- optionally included via queryfeature_id - even though this
+-- is redundant with the hitpair table below. this can still
+-- be useful - for instance, we may have an analysis that blasts
+-- contigs against a database. we may then transform those hits
+-- into global coordinates; it can still be useful to keep a record
+-- of which contig was blasted as the query.
+--
+-- name: a way of grouping analyses. this should be a handy
+-- short identifier that can help people find an analysis they
+-- want. for instance "tRNAscan", "cDNA", "FlyPep", "SwissProt"
+--
+-- program: e.g. blastx, blastp, sim4, genscan
+-- programversion: e.g. TBLASTX 2.0MP-WashU [09-Nov-2000]
+-- algorithm: eg blast
+-- sourcename: eg cDNA, SwissProt
+--
+-- MAPPING (bioperl): maps to Bio::Search::Result::ResultI
 
 create table analysis (
     analysis_id serial not null,
@@ -43,8 +43,8 @@ create table analysis (
     timelastmod timestamp not null default current_timestamp
 );
 
-# analyses can have various properties attached - eg the parameters
-# used in running a blast
+-- analyses can have various properties attached - eg the parameters
+-- used in running a blast
 create table analysisprop (
     analysis_id int not null,
     foreign key (analysis_id) references analysis (analysis_id),
@@ -56,7 +56,7 @@ create table analysisprop (
     timelastmod timestamp not null default current_timestamp
 );
 
-# MAPPING (bioperl): Bio::Search::Hit::HitI
+-- MAPPING (bioperl): Bio::Search::Hit::HitI
 create table hitpair (
     hitpair_id serial not null,
     primary key (featurepair_id),
@@ -73,7 +73,7 @@ create table hitpair (
     timelastmod timestamp not null default current_timestamp
 );
 
-# MAPPING (bioperl): Bio::Search::HSP::HSPI
+-- MAPPING (bioperl): Bio::Search::HSP::HSPI
 create table hsp (
     hsp_id serial not null,
     hitpair_id int not null,
@@ -112,7 +112,7 @@ create table hspprop (
     timelastmod timestamp not null default current_timestamp
 );
 
-# NOT YET STABLE
+-- NOT YET STABLE
 create table multalign (
     multalign_id serial not null,
     primary key (multalign_id),
