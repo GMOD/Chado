bc6cd357b21000eeaa2474fc291fa366c4ff5e06
cmungall <cmungall@224a875b-6a50-0410-9993-82261b5d0d45>
2002-12-03 08:05:46 +0000

    *** empty log message ***

--- schema/trunk/chado/modules/sequence/sequence.sql
+++ schema/trunk/chado/modules/sequence/sequence.sql
@@ -10,34 +10,103 @@ create table feature (
        foreign key (dbxref_id) references dbxref (dbxref_id),
        
        name varchar(255) not null,
-       fnbeg int,
-       fnend int,
-       strand smallint,
        residues text,
        seqlen int,
        md5checksum char(32),
        type_id int,
        foreign key (type_id) references cvterm (cvterm_id),
-       source_feature_id int,
-       foreign key (source_feature_id) references feature (feature_id),
        timeentered timestamp not null default current_timestamp,
        timelastmod timestamp not null default current_timestamp,
 
-       unique(dbxref_id),
-       unique(name, fnbeg, fnend, strand, source_feature_id, type_id)
+       unique(dbxref_id)
 );
+
+-- ================================================
+-- TABLE: featureloc
+-- ================================================
+
+-- each feature can have 0 or more locations.
+-- multiple locations do NOT indicate non-contiguous locations.
+-- instead they designate alternate locations or grouped locations;
+-- for instance, a feature designating a blast hit or hsp will have two
+-- locations, one on the query feature, one on the subject feature.
+-- features representing sequence variation could have alternate locations
+-- instantiated on a feature on the mutant strain.
+-- the field "rank" is used to differentiate these different locations.
+-- the default rank '0' is used for the main/primary location (eg in
+-- similarity features, 0 is query, 1 is subject), although sometimes
+-- this will be symmeytical and there is no primary location.
+--
+-- redundant locations can also be stored - for instance, the position
+-- of an exon on a BAC and in global coordinates. the field "locgroup"
+-- is used to differentiate these groupings of locations. the default
+-- locgroup '0' is used for the main/primary location, from which the
+-- others can be derived via coordinate transformations. another
+-- example of redundant locations is storing ORF coordinates relative
+-- to both transcript and genome. redundant locations open the possibility
+-- of the database getting into inconsistent states; this schema gives
+-- us the flexibility of both 'warehouse' instantiations with redundant
+-- locations (easier for querying) and 'management' instantiations with
+-- no redundant locations.
+
+-- most features (exons, transcripts, etc) will have 1 location, with
+-- locgroup and rank equal to 0
+--
+-- an example of using both locgroup and rank:
+-- imagine a feature indicating a conserved region between the chromosomes
+-- of two different species. we may want to keep redundant locations on
+-- both contigs and chromosomes. we would thus have 4 locations for the
+-- single conserved region feature - two distinct locgroups (contig level
+-- and chromosome level) and two distinct ranks (for the two species).
+
+-- altresidues is used to store alternate residues of a feature, when these
+-- differ from feature.residues. for instance, a SNP feature located on
+-- a wild and mutant protein would have different alresidues.
+-- for alignment/similarity features, the altresidues is used to represent
+-- the alignment string.
+
+-- note on variation features; even if we don't want to instantiate a mutant
+-- chromosome/contig feature, we can still represent a SNP etc with 2 locations,
+-- one (rank 0) on the genome, the other (rank 1) would have most fields null,
+-- except for altresidues
+
 -- IMPORTANT: fnbeg and fnend are space-based (INTERBASE) coordinates
 -- this is vital as it allows us to represent zero-length
 -- features eg splice sites, insertion points without
 -- an awkward fuzzy system
 
--- fnbeg, fnend are for feature natural begin/end
+-- nbeg, nend are for feature natural begin/end
 -- by natural begin, end we mean these are the actual
 -- beginning (5' position) and actual end (3' position)
 -- rather than the low position and high position, as
 -- these terms are sometimes erroneously used
 
--- seqlen is redundant except for transcripts (where we're being ambiguous)
+create table featureloc (
+       featureloc_id serial not null,
+       primary key (featureloc_id),
+
+       feature_id int not null,
+       foreign key (feature_id) references feature (feature_id),
+
+       nbeg int,
+       nend int,
+       strand smallint,
+       srcfeature_id int,
+       foreign key (srcfeature_id) references feature (feature_id),
+
+       altresidues text,
+
+       locgroup int not null default 0,
+       rank     int not null default 0,
+
+       timeentered timestamp not null default current_timestamp,
+       timelastmod timestamp not null default current_timestamp,
+
+       unique (feature_id, srcfeature_id),
+       unique (feature_id, locgroup, rank)
+
+);
+
 
 -- ================================================
 -- TABLE: feature_pub
