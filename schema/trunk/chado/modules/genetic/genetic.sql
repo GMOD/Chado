2d176e85d936f38129ebfffb238a4fe21111b7e8
allenday <allenday@224a875b-6a50-0410-9993-82261b5d0d45>
2002-12-03 03:07:31 +0000

    changing ## comments to --.  the ##s didn't sit well with my version of postgres (redhat rpm 7.2.1-5)

--- schema/trunk/chado/modules/genetic/genetic.sql
+++ schema/trunk/chado/modules/genetic/genetic.sql
@@ -1,8 +1,8 @@
-## This module depends on the sequence, pub, and cv modules 
+-- This module depends on the sequence, pub, and cv modules 
 
-## ================================================
-## TABLE: genotype
-## ================================================
+-- ================================================
+-- TABLE: genotype
+-- ================================================
 
 create table genotype (
        genotype_id serial not null,
@@ -13,9 +13,9 @@ create table genotype (
 );
 
 
-## ================================================
-## TABLE: feature_genotype
-## ================================================
+-- ================================================
+-- TABLE: feature_genotype
+-- ================================================
 
 create table feature_genotype (
        feature_id int,
@@ -26,30 +26,30 @@ create table feature_genotype (
        timelastmod timestamp not null default current_timestamp
 );
 
-## ================================================
-## TABLE: phenotype
-## ================================================
+-- ================================================
+-- TABLE: phenotype
+-- ================================================
 
 create table phenotype (
        phenotype_id serial not null,
        primary key (phenotype_id),
        description text,
-## type of phenotypic statement  [Chris, we need this or something like it
-## for FB where we have three types of statement in *k: "Phenotypic class:",
-## "Phenotype manifest in:", and free-text]
+-- type of phenotypic statement  [Chris, we need this or something like it
+-- for FB where we have three types of statement in *k: "Phenotypic class:",
+-- "Phenotype manifest in:", and free-text]
        statement_type int not null,
        foreign key (statement_type) references cvterm (cvterm_id),
        pub_id int not null,
        foreign key (pub_id) references pub (pub_id),
-## Do we want to call this simply genotype_id to allow natural joins?
+-- Do we want to call this simply genotype_id to allow natural joins?
        background_genotype_id int,
        foreign key (background_genotype_id) references genotype (genotype_id),
        timeentered timestamp not null default current_timestamp,
        timelastmod timestamp not null default current_timestamp
 );
-## ================================================
-## TABLE: feature_phenotype
-## ================================================
+-- ================================================
+-- TABLE: feature_phenotype
+-- ================================================
 
 create table feature_phenotype (
        feature_id int not null,
@@ -60,9 +60,9 @@ create table feature_phenotype (
        timelastmod timestamp not null default current_timestamp
 );
 
-## ================================================
-## TABLE: phenoype_cvterm
-## ================================================
+-- ================================================
+-- TABLE: phenoype_cvterm
+-- ================================================
 
 create table phenotype_cvterm (
        phenotype_id int not null,
@@ -75,9 +75,9 @@ create table phenotype_cvterm (
 );
 
 
-## ================================================
-## TABLE: interaction
-## ================================================
+-- ================================================
+-- TABLE: interaction
+-- ================================================
 
 create table interaction (
        interaction_id serial not null,
@@ -85,7 +85,7 @@ create table interaction (
        description text,
        pub_id int not null,
        foreign key (pub_id) references pub (pub_id),
-## Do we want to call this simply genotype_id to allow natural joins?
+-- Do we want to call this simply genotype_id to allow natural joins?
        background_genotype_id int,
        foreign key (background_genotype_id) references genotype (genotype_id),
        phenotype_id int,
@@ -95,9 +95,9 @@ create table interaction (
 );
 
 
-## ================================================
-## TABLE: interaction_subj
-## ================================================
+-- ================================================
+-- TABLE: interaction_subj
+-- ================================================
 
 create table interaction_subj (
        feature_id int not null,
@@ -109,9 +109,9 @@ create table interaction_subj (
 );
 
 
-## ================================================
-## TABLE: interaction_obj
-## ================================================
+-- ================================================
+-- TABLE: interaction_obj
+-- ================================================
 
 create table interaction_obj (
        feature_id int not null,
