9bf76b91827938b0638b2c92433efa40384a7fc9
David Emmert <emmert@users.sourceforge.net>
2003-01-27 19:34:34 +0000

    *** empty log message ***

--- chado/modules/companalysis/companalysis.sql
+++ chado/modules/companalysis/companalysis.sql
@@ -1,4 +1,4 @@
-## $Id: companalysis.sql,v 1.15 2003-01-21 21:58:39 emmert Exp $
+## $Id: companalysis.sql,v 1.16 2003-01-27 19:34:34 emmert Exp $
 
 -- ================================================
 -- TABLE: analysis
@@ -63,7 +63,7 @@ create table analysis (
     queryfeature_id int,
     foreign key (queryfeature_id) references feature (feature_id),
 
-    unique(program, programversion)
+    unique(program, programversion,sourcename)
 );
 create index analysis_idx1 on analysis (queryfeature_id);
 
