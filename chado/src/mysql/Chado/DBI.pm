44d2fffe0b89d34c2cc83d36753361544164dc96
Allen Day <allenday@users.sourceforge.net>
2003-02-22 02:01:44 +0000

    funky bug caused by the new db table (which i have not yet commited as sql...
    also added the ability to define the username/password used to connect in Chado::DBI

--- chado/src/mysql/Chado/DBI.pm
+++ chado/src/mysql/Chado/DBI.pm
@@ -1,10 +1,44 @@
 package Chado::DBI;
+
+my $USER ||= 'nobody';
+my $PASS ||= '';
+
 use base 'Class::DBI::mysql';
+
 Chado::DBI->set_db(
-				   'Main',
-				   'dbi:mysql:chado_wax;host=sumo',
-				   'nobody',
-				   ''
-				  );
+			'Main',
+			'dbi:mysql:chado_wax;host=sumo',
+			$USER,
+			$PASS,
+		  );
+
+sub _set_db {
+
+  warn $USER;
+  warn $PASS;
+
+  Chado::DBI->set_db(
+			'Main',
+			'dbi:mysql:chado_wax;host=sumo',
+			$USER,
+			$PASS,
+		    );
+}
+
+sub user {
+warn "user()";
+  my $pack = shift;
+  $USER = shift if @_;
+  _set_db();
+  return $USER;
+}
+
+sub pass {
+warn "pass()";
+  my $pack = shift;
+  $PASS = shift if @_;
+  _set_db();
+  return $PASS;
+}
 
 1;
