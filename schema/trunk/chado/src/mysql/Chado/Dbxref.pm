db21e948a8067c803df18435e5f7a66f4b853016
allenday <allenday@224a875b-6a50-0410-9993-82261b5d0d45>
2003-02-22 02:01:44 +0000

    funky bug caused by the new db table (which i have not yet commited as sql...
    also added the ability to define the username/password used to connect in Chado::DBI

--- schema/trunk/chado/src/mysql/Chado/Dbxref.pm
+++ schema/trunk/chado/src/mysql/Chado/Dbxref.pm
@@ -2,7 +2,10 @@ package Chado::Dbxref;
 use base 'Chado::DBI';
 
 Chado::Dbxref->set_up_table('dbxref');
+Chado::Dbxref->hasa(Chado::Db => 'db_id');
+
 sub id { return shift->dbxref_id }
 sub name { return shift->accession }
+sub db { return shift->db_id }
 
 1;
