6eea97ac2a0f01567f43c02567be73d395424c03
Allen Day <allenday@users.sourceforge.net>
2003-02-24 21:43:40 +0000

    adding another cvterm link.

--- chado/src/mysql/Chado/Expression_Cvterm.pm
+++ chado/src/mysql/Chado/Expression_Cvterm.pm
@@ -6,6 +6,7 @@ Chado::Expression_Cvterm->set_up_table('expression_cvterm');
 Chado::Expression_Cvterm->hasa(Chado::Expression => 'expression_id');
 Chado::Expression_Cvterm->hasa(Chado::Cvterm     => 'cvterm_id');
 
+sub id { return shift->expression_cvterm_id }
 sub expression { return shift->expression_id }
 sub cvterm     { return shift->cvterm_id     }
 
