c7eb6137c0975f8e4bf5d572f275c0ef8d7fda1e
allenday <allenday@224a875b-6a50-0410-9993-82261b5d0d45>
2003-03-04 00:41:08 +0000

    importing the "tableinfo" table from gus-core.  this is necessary for some of the tuple keys in the rad module.

--- schema/trunk/chado/modules/general/general.sql
+++ schema/trunk/chado/modules/general/general.sql
@@ -13,4 +13,21 @@ create table dbxref (
        unique (dbname, accession, version)
 );
 
+-- ================================================
+-- TABLE: tableinfo
+-- ================================================
 
+create table tableinfo (
+       table_id serial not null,
+       primary key (table_id),
+       name varchar(30) not null,
+       table_type varchar(40) not null,
+       primary_key_column varchar(30) null,
+       database_id int(5) not null,
+       is_versioned int(1) not null,
+       is_view int(1) not null,
+       view_on_table_id int(5) null,
+       superclass_table_id int(5) null,
+       is_updateable int(1) not null,
+       modification_date date not null
+);
