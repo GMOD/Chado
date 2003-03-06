55793b0c3c3516440f2d8c58b38df8b4bb172994
Allen Day <allenday@users.sourceforge.net>
2003-03-06 01:43:40 +0000

    dropping a debug statement

--- chado/src/mysql/Chado/Expression_Egroup.pm
+++ chado/src/mysql/Chado/Expression_Egroup.pm
@@ -4,7 +4,7 @@ use mixin 'Class::DBI::Join';
 
 Chado::Expression_Egroup->set_up_table('expression_egroup');
 Chado::Expression_Egroup->hasa(Chado::Expression => 'expression_id');
-Chado::Expression_Egroup->hasa(Chado::EGroup     => 'egroup_id');
+Chado::Expression_Egroup->hasa(Chado::Egroup     => 'egroup_id');
 
 sub expression { return shift->expression_id }
 sub egroup     { return shift->egroup_id     }
