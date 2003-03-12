8f5cc3259be8c1f85a09d006299f2d61da4688a4
Allen Day <allenday@users.sourceforge.net>
2003-03-12 01:55:17 +0000

    fixing some problems caused by rad import

--- chado/modules/general/general.sql
+++ chado/modules/general/general.sql
@@ -5,9 +5,9 @@
 create table dbxref (
        dbxref_id serial not null,
        primary key (dbxref_id),
-       dbname varchar(85) not null,
-       accession varchar(85) not null,
-       version varchar(85) not null default '',
+       dbname varchar(255) not null,
+       accession varchar(255) not null,
+       version varchar(255) not null default '',
        dbxrefdescription text,
 
        unique (dbname, accession, version)
@@ -19,16 +19,16 @@ create table dbxref (
 
 create table tableinfo (
        tableinfo_id serial not null,
-       primary key (table_id),
+       primary key (tableinfo_id),
        name varchar(30) not null,
        table_type varchar(40) not null,
        primary_key_column varchar(30) null,
-       database_id int(5) not null,
-       is_versioned int(1) not null,
-       is_view int(1) not null,
-       view_on_table_id int(5) null,
-       superclass_table_id int(5) null,
-       is_updateable int(1) not null,
+       database_id int not null,
+       is_versioned int not null,
+       is_view int not null,
+       view_on_table_id int null,
+       superclass_table_id int null,
+       is_updateable int not null,
        modification_date date not null
 );
 
@@ -36,8 +36,8 @@ create table tableinfo (
 -- TABLE: projectinfo
 -- ================================================
 create table projectinfo (
-       projectinfo_id not null serial,
+       projectinfo_id serial not null,
        primary key (projectinfo_id),
        name varchar(255) not null,
-       description varchar(255) not null,
+      description varchar(255) not null
 );
