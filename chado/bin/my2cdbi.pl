7a40cdc0e7a829732640f2afd856cafd78a3adfa
Allen Day <allenday@users.sourceforge.net>
2003-04-23 00:11:34 +0000

    autogenerate my and pg classdbi pm files

---
+++ chado/bin/my2cdbi.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/perl
+use strict;
+use SQL::Translator;
+use SQL::Translator::Producer::ClassDBI;
+
+my @files = @ARGV;
+
+my $translator     = SQL::Translator->new();
+
+my $config = { from       => "MySQL", 
+	       to         => "ClassDBI",
+	       filename   => \@files,
+	     };
+
+$translator->format_package_name(\&x);
+$translator->format_pk_name(sub {return 'id';});
+$translator->format_fk_name(\&y);
+
+my $output = $translator->translate($config) or die $translator->error;
+
+print $output;
+
+
+sub x { 
+my ($name, $primary_key) = @_;
+
+my $package_name;
+
+my @temp = split(/_/,$name);
+
+for(my $i = 0; $i < scalar(@temp); $i++) {
+  my $new_name = ucfirst($temp[$i]);
+
+  if($i == 0) {
+	$package_name .= $new_name;
+  }else {
+	$package_name .= "_" .$new_name;
+  }
+
+}
+
+
+$package_name = 'Chado::' . $package_name;
+
+return $package_name;
+
+}
+
+sub y {
+  my $table_name = shift;
+  return $table_name;
+}
+
