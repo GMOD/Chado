2227e750ba0bc75a69071fb017be4aefcf322752
allenday <allenday@224a875b-6a50-0410-9993-82261b5d0d45>
2003-04-24 22:24:36 +0000

    adding ability to generate diagrams from schema

---
+++ schema/trunk/chado/bin/pg2graphviz.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+use SQL::Translator;
+use Data::Dumper;   
+
+$SQL::Translator::DEBUG = 0;
+
+my $in = shift;
+open(IN,$in) || die "couldn't open $in: $!";
+
+my @create = <IN>;
+my $create = join '', @create;
+
+my $tr = SQL::Translator->new(
+				parser   => "PostgreSQL",
+				producer => "GraphViz"
+                             );
+                               
+print $tr->translate(\$create);
