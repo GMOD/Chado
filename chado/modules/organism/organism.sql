7790c9d62d9c66f7ce67ad93bfd6c8c815d7b89c
David Emmert <emmert@users.sourceforge.net>
2003-05-19 12:46:44 +0000

    added organismprop table.  deleted taxgroup field from organism (this will be stored as an organismprop)

--- chado/modules/organism/organism.sql
+++ chado/modules/organism/organism.sql
@@ -7,12 +7,11 @@ create table organism (
 	primary key (organism_id),
 	abbrev varchar(255) null,
 	genus varchar(255) not null,
-	taxgroup varchar(255) not null,
 	species varchar(255) not null,
 	common_name varchar(255) null,
 	comment text null,
 
-	unique(taxgroup, genus, species)
+	unique(genus, species)
 );
 -- Compared to mol5..Species, organism table lacks "approved char(1) null".  
 -- We need to work w/ Aubrey & Michael to ensure that we don't need this in 
@@ -22,6 +21,7 @@ create table organism (
 -- if it's really necessary we can have an organismprop table
 -- for adding internal project specific data
 -- [cjm]
+-- done (below) 19-MAY-03 [dave]
 
 
 -- ================================================
@@ -40,4 +40,23 @@ create table organism_dbxref (
 );
 create index organism_dbxref_idx1 on organism_dbxref (organism_id);
 create index organism_dbxref_idx2 on organism_dbxref (dbxref_id);
+
+-- ================================================
+-- TABLE: organismprop
+-- ================================================
+
+create table organismprop (
+       organismprop_id serial not null,
+       primary key (organismprop_id),
+       organism_id int not null,
+       foreign key (organism_id) references organism (organism_id),
+       pkey_id int not null,
+       foreign key (pkey_id) references cvterm (cvterm_id),
+       pval text not null default '',
+       prank int not null default 0,
+
+       unique(organism_id, pkey_id, pval, prank)
+);
+create index organismprop_idx1 on organismprop (organism_id);
+create index organismprop_idx2 on organismprop (pkey_id);
 
