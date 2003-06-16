3c06a47162ee806595af5ae32398065a4e6ffcd1
allenday <allenday@224a875b-6a50-0410-9993-82261b5d0d45>
2003-06-16 19:51:24 +0000

    adding createdb and dropdb statements.  added variable for db name

--- schema/trunk/chado/bin/test_load.sh
+++ schema/trunk/chado/bin/test_load.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-cat modules/complete.sql | psql test 2>&1 | grep -E 'ERROR|FATAL';
+DBNAME=chado_gonzo;
+
+dropdb $DBNAME;
+createdb $DBNAME;
+cat modules/complete.sql | psql $DBNAME 2>&1 | grep -E 'ERROR|FATAL|No such file or directory';
+echo "database $DBNAME created";
 true;
