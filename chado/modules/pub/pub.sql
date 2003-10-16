f26dbdf420001763b188b290778275781364f56b
Allen Day <allenday@users.sourceforge.net>
2003-10-16 18:40:47 +0000

    merging tag allenday changes to HEAD.  changes in pub module are:
    author_pub.arank renamed to author_pub.rank
    added a nullable constraint on author table to contact table

--- chado/modules/pub/pub.sql
+++ chado/modules/pub/pub.sql
@@ -15,15 +15,14 @@ create table pub (
        issue  varchar(255),
        pyear  varchar(255),
        pages  varchar(255),
-       miniref varchar(255),
-       uniquename text not null,
+       miniref varchar(255) not null,
        type_id int not null,
        foreign key (type_id) references cvterm (cvterm_id) on delete cascade,
        is_obsolete boolean default 'false',
        publisher varchar(255),
        pubplace varchar(255),
 
-       unique(uniquename)
+       unique(miniref)
 );
 -- title: title of paper, chapter of book, journal, etc
 -- volumetitle: title of part if one of a series
@@ -31,11 +30,6 @@ create table pub (
 -- pages: page number range[s], eg, 457--459, viii + 664pp, lv--lvii
 -- type_id: the type of the publication (book, journal, poem, graffiti, etc)
 -- is_obsolete: do we want this even though we have the relationship in pub_relationship?
--- uniquename: the unique name for a publication. FlyBase policy for setting publication 
--- 	uniquenames is the following:
---	a. use MEDLINE id for any publication that has one
---	b. for books/journals, use the ISSN number
---	c. for anything else, use FBrf#
 create index pub_idx1 on pub (type_id);
 
 -- ================================================
@@ -93,6 +87,8 @@ create index pub_dbxref_idx2 on pub_dbxref (dbxref_id);
 create table author (
        author_id serial not null,
        primary key (author_id),
+       contact_id int null,
+       foreign key (contact_id) references contact (contact_id),
        surname varchar(100) not null,
        givennames varchar(100),
        suffix varchar(100),
@@ -114,12 +110,12 @@ create table pub_author (
        foreign key (author_id) references author (author_id) on delete cascade,
        pub_id int not null,
        foreign key (pub_id) references pub (pub_id) on delete cascade,
-       arank int not null,
+       rank int not null,
        editor boolean default 'false',
 
        unique(author_id,pub_id)
 );
--- arank: order of author in author list for this pub
+-- rank: order of author in author list for this pub
 -- editor: indicates whether the author is an editor for linked publication
 create index pub_author_idx1 on pub_author (author_id);
 create index pub_author_idx2 on pub_author (pub_id);
