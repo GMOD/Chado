c42bb46af1eb8faab525ca73674248810274fe09
Allen Day <allenday@users.sourceforge.net>
2003-10-24 23:12:57 +0000

    ontology loading is now automated.  all files are in cvs, or expect more commits.

--- chado/lib/Chado/Builder.pm
+++ chado/lib/Chado/Builder.pm
@@ -4,6 +4,7 @@ use base 'Module::Build';
 use Data::Dumper;
 use Template;
 use XML::Simple;
+use LWP::Simple qw(mirror is_success status_message);
 
 =head1 ACTIONS
 
@@ -31,8 +32,69 @@ sub ACTION_foo {
 
 sub ACTION_ontologies {
   my $m = shift;
-  warn $m;
-  warn Dumper($m->conf);
+
+  my $conf = $m->conf;
+
+  print "Available ontologies:\n";
+
+  my $i = 1;
+  my %ont = ();
+  foreach my $ontology (sort keys %{ $conf->{ontology} }) {
+    $ont{$i} = $ontology;
+    print "[$i] $ontology\n";
+    $i++;
+  }
+  print "\n";
+
+  my $chosen = $m->prompt("Which ontologies would you like to load (Comma delimited)? [0]");
+  $m->notes('ontologies' => $chosen);  
+  
+  my %ontologies = map {$ont{$_} => $conf->{ontology}{$ont{$_}}} split ',',$chosen;
+
+  foreach my $ontology (keys %ontologies){
+    print "fetching files for $ontology\n";
+
+	my $load = 0;
+    foreach my $file (@{ $ontologies{$ontology}{file} }){
+      print "  +",$file->{remote},"\n";
+
+      my $fullpath = $conf->{path}{data}.'/'.$file->{local};
+      $fullpath =~ s!^(.+)/[^/]*!$1!;
+
+      if(! -d $fullpath){
+        system( 'mkdir','-p',$fullpath ) or print "!possible problem, couldn't mkdir -p $fullpath: $!\n";
+      }
+
+      #mirror the file
+      my $rc = mirror($file->{remote}, $conf->{path}{data} .'/'. $file->{local});
+
+      if ($rc == 304) {
+        print "    ". $file->{local} ." is up to date\n";
+      } elsif (!is_success($rc)) {
+        print "    $rc ", status_message($rc), "   (",$file->{remote},")\n";
+        next;
+      } else {
+		#file is new, load it
+        print "    updated\n";
+		$load = 1;
+      }
+    }
+	next unless $load;
+
+	my($deffile) = grep {$_ if $_->{type} eq 'definitions'} @{ $ontologies{$ontology}{file} };
+	foreach my $ontfile (grep {$_->{type} eq 'ontology'} @{ $ontologies{$ontology}{file} }){
+      print "    loading...";
+	  system('./bin/load_ontology.pl',
+			 $conf->{'tt2'}{'load/tt2/AutoDB.tt2'}{'token'}{'db_username'},
+			 $conf->{'tt2'}{'load/tt2/AutoDB.tt2'}{'token'}{'db_name'},
+			 $conf->{'path'}{'data'}.'/'.$ontfile->{'local'},
+			 $conf->{'path'}{'data'}.'/'.$deffile->{'local'},
+			) && (print "failed: $!\n" and die);
+	  print "done!\n";
+
+#warn Dumper($conf);
+	}
+  }
 }
   
 sub ACTION_tokenize {
@@ -58,13 +120,21 @@ sub ACTION_tokenize {
 
 =cut
 
+sub property {
+  my $self = shift;
+  my $key  = shift;
+  my $val  = $self->{properties}{$key};
+  $val     =~ s/^$key=//;
+  return $val;
+}
+
 sub conf {
   my $self = shift;
-  my $arg = shift;
-#  $self->{conf} = XMLin($arg, forcearray => ['token'], keyattr => [ 'tt2', 'input', 'token', 'name', 'value'], ContentKey => '-value') if defined($arg);
-  $self->{conf} = XMLin($arg, forcearray => ['token','path'], keyattr => [qw(tt2 input token name
-  )], ContentKey => '-value') if defined($arg);
-warn Dumper($self->{conf});
+  return $self->{conf} if defined $self->{conf};
+
+  my $file = $self->property('load_conf');
+  $self->{conf} = XMLin($file, forcearray => ['token','path','file'], keyattr => [qw(tt2 input token name file)], ContentKey => '-value');
+#warn Dumper($self->{conf});
   return $self->{conf};
 }
 
