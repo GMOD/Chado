b1685ff22db90c495e3d2082d5c312cf3621cc34
boconnor <boconnor@224a875b-6a50-0410-9993-82261b5d0d45>
2003-11-05 07:25:16 +0000

    I use this for inserts into the database during the load process.  You can trigger these inserts by calling ./Build prepdb.  Keep in mind the contents should be changed depending on what you're loading into the database.

---
+++ schema/trunk/chado/load/etc/initialize.sql
@@ -0,0 +1,6 @@
+/* For load_gff3.pl */
+insert into organism (abbreviation, genus, species, common_name)
+       values ('H.sapiens', 'Homo','sapiens','Human');
+insert into contact (name) values ('DBUSER');
+insert into db (name, contact_id) values ('refseq',1);
+
