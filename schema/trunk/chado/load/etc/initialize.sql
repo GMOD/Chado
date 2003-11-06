f21a3241b5779d485ebe3487a0044c269bb7068f
allenday <allenday@224a875b-6a50-0410-9993-82261b5d0d45>
2003-11-06 22:08:39 +0000

    standardize

--- schema/trunk/chado/load/etc/initialize.sql
+++ schema/trunk/chado/load/etc/initialize.sql
@@ -2,5 +2,5 @@
 insert into organism (abbreviation, genus, species, common_name)
        values ('H.sapiens', 'Homo','sapiens','Human');
 insert into contact (name) values ('DBUSER');
-insert into db (name, contact_id) values ('refseq',1);
+insert into db (name, contact_id) values ('DB:refseq',1);
 
