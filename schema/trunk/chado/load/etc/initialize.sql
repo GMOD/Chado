4d01789c2d5b9aae3ab2b2b21f19cae5ba677708
allenday <allenday@224a875b-6a50-0410-9993-82261b5d0d45>
2003-11-08 02:30:33 +0000

    more stuff

--- schema/trunk/chado/load/etc/initialize.sql
+++ schema/trunk/chado/load/etc/initialize.sql
@@ -2,5 +2,8 @@
 insert into organism (abbreviation, genus, species, common_name)
        values ('H.sapiens', 'Homo','sapiens','Human');
 insert into contact (name) values ('DBUSER');
+insert into cv (name) values ('null');
+insert into cvterm (name,cv_id) values ('null',(select cv_id from cv where name = 'null'));
+insert into pub (miniref,type_id) values ('null',(select cvterm_id from cvterm where name = 'null'));
 insert into db (name, contact_id) values ('DB:refseq',1);
 
