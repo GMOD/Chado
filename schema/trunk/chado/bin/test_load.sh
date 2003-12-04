f9fcd1a5de072c86fea4bfb6c9ead9af4c385c97
kycl4rk <kycl4rk@224a875b-6a50-0410-9993-82261b5d0d45>
2003-12-04 22:34:51 +0000

    Removing this as it has been superceded by "test_load.pl."

--- schema/trunk/chado/bin/test_load.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-DBNAME=$1;
-
-dropdb $DBNAME;
-createdb $DBNAME;
-cat modules/complete.sql | psql $DBNAME 2>&1 | grep -E 'ERROR|FATAL|No such file or directory';
-echo "database $DBNAME created";
-true;
