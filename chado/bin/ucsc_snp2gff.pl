3c8bf84f8c2d09cd9e1afffc1902685cb07ce0c7
Allen Day <allenday@users.sourceforge.net>
2004-05-05 19:30:18 +0000

    processes http://www.genome.ucsc.edu/goldenPath/hg16/database/snpMap.txt.gz into gff3.  needs POD added.

---
+++ chado/bin/ucsc_snp2gff.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+# convert UCSC gene files into GFF3 data
+
+use strict;
+use File::Basename 'basename';
+use Getopt::Long;
+use URI::Escape;
+use Text::Wrap;
+$Text::Wrap::columns = 79;
+use Bio::SeqIO;
+use Bio::SeqFeature::Generic;
+use Data::Dumper;
+
+while(<>){
+  chomp;
+  my($bin,$chrom,$start,$end,$name,$source,$type) = split /\t/;
+
+  my $gfftype = $type eq 'SNP' ? 'SNP' :
+                $type eq 'INDEL' ? 'indel' :
+                $type eq 'SEGMENTAL' ? 'simple_sequence_length_polymorphism' :
+                $type eq 'unknown' ? 'sequence_variant' :
+                die "don't know how to represent variant type $type";
+
+  print join("\t", ($chrom, $source, $gfftype, $start + 1, $end, '.', '.', '.', "ID=$name")), "\n";
+}
