9939184941866ea728d9c2a764d4f4fe39fd62c3
scottcain <scottcain@224a875b-6a50-0410-9993-82261b5d0d45>
2004-08-13 15:00:24 +0000

    if we support non-local dbs (which we should), we should also support non-standard ports

--- schema/trunk/chado/bin/test_load.sh
+++ schema/trunk/chado/bin/test_load.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 DBHOST=$1
-DBNAME=$2;
+DBPORT=$2
+DBNAME=$3;
 
-dropdb -h $DBHOST $DBNAME;
-createdb -h $DBHOST $DBNAME;
-cat modules/complete.sql | psql -h $DBHOST $DBNAME 2>&1 | grep -E 'ERROR|FATAL|No such file or directory';
-echo "database $DBNAME created on $DBHOST";
+dropdb -h $DBHOST -p $DBPORT $DBNAME;
+createdb -h $DBHOST -p $DBPORT $DBNAME;
+cat modules/complete.sql | psql -h $DBHOST -p $DBPORT $DBNAME 2>&1 | grep -E 'ERROR|FATAL|No such file or directory';
+echo "database $DBNAME created on $DBHOST:$DBPORT";
 true;
