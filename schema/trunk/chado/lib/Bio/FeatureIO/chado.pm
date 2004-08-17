f1fade2c8d1f3c72aad0774ebf7fb77a3e46a284
allenday <allenday@224a875b-6a50-0410-9993-82261b5d0d45>
2004-08-17 06:25:12 +0000

    new classes for loading sequence feature data to chado
    1) chado.pm -- will be a revamp of the current gmod_load_gff3.pl using
    Class::DBI based load.
    
    2) chadobulk.pm -- will be written from the ground up to lock tables,
    create bulk import files, load them to chado, and unlock.
    
    i've already written enough into chadobulk.pm to get data loaded into the
    feature table.  chado.pm doesn't do anything yet.
    
    this all requires bioperl-live HEAD.

---
+++ schema/trunk/chado/lib/Bio/FeatureIO/chado.pm
@@ -0,0 +1,92 @@
+package Bio::FeatureIO::chado;
+
+use strict;
+use base qw(Bio::FeatureIO);
+
+use Bio::SeqIO;
+use Chado::LoadDBI;
+use Data::Dumper;
+
+sub _initialize {
+  my($self,%arg) = @_;
+  $self->SUPER::_initialize(%arg);
+
+  $self->feature_count(0);
+  $self->organism($arg{-organism} || 'Human');
+  $self->cachesize($arg{-cachesize} || 1000);
+}
+
+sub next_feature {
+  shift->throw('this class only writes to database');
+}
+
+sub write_feature {
+  my($self,$feature) = shift;
+
+}
+
+=head2 cache
+
+ Title   : cache
+ Usage   : $obj->cache($newval)
+ Function: cache an object for commit to db.  when number of
+           items in cache exceeds cachesize(), objects are flushed
+ Example : 
+ Returns : value of cache (a scalar)
+ Args    : on set, new value (a scalar or undef, optional)
+
+
+=cut
+
+sub cache{
+  my $self = shift;
+  my $val = shift;
+
+  push @{ $self->{'cache'} }, $val if ref($val);
+
+  if(scalar @{ $self->{'cache'} } > $self->cachesize){
+    $_->dbi_commit foreach @{ $self->{'cache'} };
+    @{ $self->{'cache'} } = ();
+  }
+}
+
+=head2 cachesize
+
+ Title   : cachesize
+ Usage   : $obj->cachesize($newval)
+ Function: number of features to cache before flushing to db
+ Example : 
+ Returns : value of cachesize (a scalar)
+ Args    : on set, new value (a scalar or undef, optional)
+
+
+=cut
+
+sub cachesize{
+    my $self = shift;
+
+    return $self->{'cachesize'} = shift if @_;
+    return $self->{'cachesize'};
+}
+
+
+=head2 organism
+
+ Title   : organism
+ Usage   : $obj->organism($newval)
+ Function: organism of features being loaded
+ Example : 
+ Returns : value of organism (a scalar)
+ Args    : on set, new value (a scalar or undef, optional)
+
+
+=cut
+
+sub organism{
+    my $self = shift;
+
+    return $self->{'organism'} = shift if @_;
+    return $self->{'organism'};
+}
+
+1;
