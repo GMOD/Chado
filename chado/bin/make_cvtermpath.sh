4305d9d74b898c70f9478422f75dce532da577fb
Scott Cain <scott@scottcain.net>
2004-08-27 02:05:46 +0000

    fixing cvtermpath.sh for current cv usage, and adding info for rice to init

--- chado/bin/make_cvtermpath.sh
+++ chado/bin/make_cvtermpath.sh
@@ -8,7 +8,11 @@ echo "Sequence Ontology"
 ./bin/make_cvtermpath.pl $USER $DBNAME 'Sequence Ontology'             | psql -q $DBNAME
 
 echo "Gene Ontology"
-./bin/make_cvtermpath.pl $USER $DBNAME 'Gene Ontology'                 | psql -q $DBNAME
+./bin/make_cvtermpath.pl $USER $DBNAME 'Cellular Component (Gene Ontology)'                 | psql -q $DBNAME
+
+./bin/make_cvtermpath.pl $USER $DBNAME 'Molecular Function (Gene Ontology)'                 | psql -q $DBNAME
+
+./bin/make_cvtermpath.pl $USER $DBNAME 'Biological Process (Gene Ontology)'                 | psql -q $DBNAME
 
 echo "Mouse Embryo Anatomy Ontology"
 ./bin/make_cvtermpath.pl $USER $DBNAME 'Mouse Embryo Anatomy Ontology' | psql -q $DBNAME
