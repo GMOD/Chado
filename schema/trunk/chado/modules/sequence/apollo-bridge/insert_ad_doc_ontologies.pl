830b9f516a25568c2e3e6106a83bebefa9c10e83
scottcain <scottcain@224a875b-6a50-0410-9993-82261b5d0d45>
2005-02-22 21:22:58 +0000

    adding a 'producedby' relationship type so apollo doesn't try (and fail) to create it on the fly.

--- schema/trunk/chado/modules/sequence/apollo-bridge/insert_ad_doc_ontologies.pl
+++ schema/trunk/chado/modules/sequence/apollo-bridge/insert_ad_doc_ontologies.pl
@@ -5,19 +5,25 @@ open ONTO, "ad_hoc_ontologies" or die "couldn't ope ad_hoc_ontologies:$!\n";
 open INSERTS, ">ontology_inserts.sql" or die "couldn't open ontology_inserts.sql: $!\n";
 
 my $cv;
-my $cvterm;
 
 while (<ONTO>) {
     if (/^#/) {
         print INSERTS "\n";
     }
-    elsif (/\[(.+)\]/) {
+    elsif (/^\[(.+)\]/) {
         $cv = $1;
         print INSERTS "INSERT INTO cv (name) VALUES ('$cv');\n";
     } elsif (/^(.+)$/) {
-        $cvterm=$1;
-        print INSERTS "INSERT INTO dbxref (db_id,accession) VALUES ((select db_id from db where name='null'), '$cv:$cvterm');\n";
-        print INSERTS "INSERT INTO cvterm (cv_id,name,dbxref_id) VALUES ((select cv_id from cv where name='$cv'),'$cvterm',(select dbxref_id from dbxref where accession='$cv:$cvterm'));\n";
+        my $term=$1;
+        if ($term =~ /(.+)\s+\[REL\]/) {
+            my $cvterm = $1;
+            print INSERTS "INSERT INTO dbxref (db_id,accession) VALUES ((select db_id from db where name='null'), '$cv:$cvterm');\n";
+            print INSERTS "INSERT INTO cvterm (cv_id,name,dbxref_id,is_relationshiptype) VALUES ((select cv_id from cv where name='$cv'),'$cvterm',(select dbxref_id from dbxref where accession='$cv:$cvterm'),1);\n";
+        } else {
+            my $cvterm = $term;
+            print INSERTS "INSERT INTO dbxref (db_id,accession) VALUES ((select db_id from db where name='null'), '$cv:$cvterm');\n";
+            print INSERTS "INSERT INTO cvterm (cv_id,name,dbxref_id) VALUES ((select cv_id from cv where name='$cv'),'$cvterm',(select dbxref_id from dbxref where accession='$cv:$cvterm'));\n";
+        }
     }
 }
 close ONTO;
