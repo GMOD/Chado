7d7f8b603d423723c16dd6bb92c3b8e2686d1a5b
Shengqiang Shu <sshu@users.sourceforge.net>
2005-05-25 21:15:11 +0000

    not needed any more

--- chado/modules/sequence/bdgp/bdgp-fix.sql
+++
@@ -1,3 +0,0 @@
-update cv set name = 'so' where name = 'SO';
-
-update feature set residues = null where exists (select 1 from cvterm where feature.type_id = cvterm.cvterm_id and cvterm.name = 'chromosome_arm');
