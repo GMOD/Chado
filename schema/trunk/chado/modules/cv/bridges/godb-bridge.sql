87a9fa7a6c9ba5703ff097b7beff5b227afde88a
allenday <allenday@224a875b-6a50-0410-9993-82261b5d0d45>
2005-06-30 06:09:33 +0000

    modified bridge to boost performance by materializing the views with many rows

--- schema/trunk/chado/modules/cv/bridges/godb-bridge.sql
+++ schema/trunk/chado/modules/cv/bridges/godb-bridge.sql
@@ -7,118 +7,209 @@ CREATE SCHEMA godb;
 
 --- helper views
 
-CREATE VIEW godb.go_acc AS
+--view
+CREATE VIEW godb.v_go_acc AS
  SELECT 
   dbxref_id                          AS dbxref_id,
   db.name || ':' || dbxref.accession AS acc
- FROM dbxref
-  INNER JOIN db USING (db_id);
+ FROM public.dbxref
+  INNER JOIN public.db USING (db_id);
+--materialized view
+CREATE TABLE godb.go_acc (
+  dbxref_id int,
+  acc text
+);
+CREATE INDEX go_acc_idx1 ON godb.go_acc (dbxref_id);
+CREATE INDEX go_acc_idx2 ON godb.go_acc (acc);
+--load
+INSERT INTO godb.go_acc SELECT * FROM godb.v_go_acc;
 
 --- dbxref [clash]
 
-CREATE VIEW godb.dbxref AS
+--view
+CREATE VIEW godb.v_dbxref AS
  SELECT
   dbxref_id                          AS id,
   db.name                            AS xref_dbname,
   dbxref.accession                   AS xref_key,
   CAST(NULL AS VARCHAR)              AS xref_keytype,
   dbxref.description                 AS xref_desc
- FROM dbxref
-  INNER JOIN db USING (db_id);
+ FROM public.dbxref
+  INNER JOIN public.db USING (db_id);
+--materialized view
+CREATE TABLE godb.dbxref (
+  id int,
+  xref_dbname varchar(255),
+  xref_key varchar(255),
+  xref_keytype varchar(255),
+  xref_desc text
+);
+CREATE INDEX dbxref_idx1 ON godb.dbxref (id);
+CREATE INDEX dbxref_idx2 ON godb.dbxref (xref_dbname);
+CREATE INDEX dbxref_idx3 ON godb.dbxref (xref_key);
+CREATE INDEX dbxref_idx4 ON godb.dbxref (xref_keytype);
+CREATE INDEX dbxref_idx5 ON godb.dbxref (xref_desc);
+--load
+INSERT INTO godb.dbxref SELECT * FROM godb.v_dbxref;
+
+
+--
+--/allen
+--
 
 -- db [clash] (note in godb, db is only refered to from association table)
 
-CREATE VIEW godb.db AS
+--view
+CREATE VIEW godb.v_db AS
  SELECT
   db_id                                 AS id,
   name                                  AS name,
   name                                  AS fullname,
   CAST(NULL AS VARCHAR)                 AS datatype,
   CAST(NULL AS VARCHAR)                 AS url_syntax
- FROM db;
+ FROM public.db;
+--materialized view
+CREATE TABLE godb.db (
+  id int,
+  name varchar(255),
+  fullname varchar(255),
+  datatype varchar(255),
+  url_syntax varchar(255)
+);
+CREATE INDEX db_idx1 ON godb.db (id);
+CREATE INDEX db_idx2 ON godb.db (name);
+CREATE INDEX db_idx3 ON godb.db (fullname);
+CREATE INDEX db_idx4 ON godb.db (datatype);
+CREATE INDEX db_idx5 ON godb.db (url_syntax);
+--load
+INSERT INTO godb.db SELECT * FROM godb.v_db;
   
 --- term
 
-CREATE VIEW term AS
+--view
+CREATE VIEW godb.v_term AS
 SELECT
  cvterm_id     AS id,
  godb.go_acc.acc AS acc,
  name          AS name,
  is_obsolete   AS is_obsolete,
  0             AS is_root
-FROM cvterm
+FROM public.cvterm
  INNER JOIN godb.go_acc USING (dbxref_id);
+--materialized_view
+CREATE TABLE godb.term (
+  id int,
+  acc text,
+  name varchar(1024),
+  is_obsolete int,
+  is_root int
+);
+CREATE INDEX term_idx1 ON godb.term (id);
+CREATE INDEX term_idx2 ON godb.term (acc);
+CREATE INDEX term_idx3 ON godb.term (name);
+CREATE INDEX term_idx4 ON godb.term (is_obsolete);
+CREATE INDEX term_idx5 ON godb.term (is_root);
+--load
+INSERT INTO godb.term SELECT * FROM godb.v_term;
 
 --- term_definition
-
-CREATE VIEW term_definition AS
+--view
+CREATE VIEW godb.term_definition AS
 SELECT
  cvterm_id AS term_id,
  definition AS term_definition
-FROM cvterm
+FROM public.cvterm
 WHERE definition IS NOT NULL;
 
 --- term_dbxref
-
-CREATE VIEW term_dbxref AS
+--view
+CREATE VIEW godb.term_dbxref AS
 SELECT
  cvterm_id AS term_id,
  dbxref_id  AS dbxref_id
-FROM cvterm_dbxref;
+FROM public.cvterm_dbxref;
 
 --- term_synonym
-
-CREATE VIEW term_synonym AS
+--view
+CREATE VIEW godb.term_synonym AS
 SELECT
  cvterm_id AS term_id,
  synonym  AS term_synonym
-FROM cvtermsynonym;
+FROM public.cvtermsynonym;
 
 --- term2term
-
-CREATE VIEW term2term AS
+--view
+CREATE VIEW godb.v_term2term AS
 SELECT
  cvterm_relationship_id AS id,
  type_id           AS relationship_type_id,
  object_id         AS term1_id,
  subject_id        AS term2_id
-FROM cvterm_relationship;
+FROM public.cvterm_relationship;
+--materialized view
+CREATE TABLE term2term (
+  id int,
+  relationship_type_id int,
+  term1_id int,
+  term2_id int
+);
+CREATE INDEX term2term_idx1 ON godb.term2term (id);
+CREATE INDEX term2term_idx2 ON godb.term2term (relationship_type_id);
+CREATE INDEX term2term_idx3 ON godb.term2term (term1_id);
+CREATE INDEX term2term_idx4 ON godb.term2term (term2_id);
+--load
+INSERT INTO godb.term2term SELECT * FROM godb.v_term2term;
 
 --- graph_path
-
-CREATE VIEW graph_path AS
+--view
+CREATE VIEW godb.v_graph_path AS
 SELECT
  cvtermpath_id      AS id,
  object_id      AS term1_id,
  subject_id     AS term2_id,
  pathdistance   AS distance
-FROM cvtermpath;
+FROM public.cvtermpath;
+--materialized view
+CREATE TABLE graph_path (
+  id int,
+  term1_id int,
+  term2_id int,
+  distance int 
+);
+CREATE INDEX graph_path_idx1 ON godb.graph_path (id);
+CREATE INDEX graph_path_idx2 ON godb.graph_path (term1_id);
+CREATE INDEX graph_path_idx3 ON godb.graph_path (term2_id);
+CREATE INDEX graph_path_idx4 ON godb.graph_path (distance);
+--load
+INSERT INTO godb.graph_path SELECT * FROM godb.v_graph_path;
 
 --Activate this to make this bridge take precedence
 --SET SEARCH PATH TO godb,public;
 
 -- species
-
-CREATE VIEW species AS
+--view
+CREATE VIEW godb.species AS
  SELECT
   organism_id                        AS id,
-  CAST(accession AS INT)             AS ncbi_taxa_id,
+--FIXME this cast does not work
+--  CAST(accession AS INT)             AS ncbi_taxa_id,
+  accession                          AS ncbi_taxa_id,
   common_name,
   CAST(NULL AS VARCHAR)              AS lineage_string,
   genus,
   species
  FROM
-  organism
-  INNER JOIN organism_dbxref USING (organism_id)
-  INNER JOIN dbxref USING (dbxref_id)
-  INNER JOIN db     USING (db_id)
+  public.organism
+  INNER JOIN public.organism_dbxref USING (organism_id)
+  INNER JOIN public.dbxref USING (dbxref_id) --schema correct?
+  INNER JOIN public.db     USING (db_id) --schema correct?
  WHERE db.name='NCBITaxon';
 
 -- gene_product
 --  note: secondary_species_id; eg for host species with parasite
 --        this will be handled by a feature_relationship in chado
-
-CREATE VIEW gene_product AS
+--view
+CREATE VIEW godb.gene_product AS
  SELECT
   feature_id                    AS id,
   name                          AS symbol,
@@ -128,79 +219,81 @@ CREATE VIEW gene_product AS
   type_id,
 -- todo: make this a left outer join on a featureprop
   name                          AS fullname
- FROM feature;
+ FROM public.feature;
 
 -- gene_product_synonym
-
-CREATE VIEW gene_product_synonym AS
+--view
+CREATE VIEW godb.gene_product_synonym AS
  SELECT
   feature_id                    AS gene_product_id,
   synonym.name                  AS product_synonym
  FROM
-  feature_synonym INNER JOIN synonym USING (synonym_id);
+  public.feature_synonym INNER JOIN public.synonym USING (synonym_id);
 
 -- gene_product_property
-
-CREATE VIEW gene_product_property AS
+--view
+CREATE VIEW godb.gene_product_property AS
  SELECT
   feature_id                    AS gene_product_id,
   prop.name                     AS property_key,
   value                         AS property_value
  FROM
-  featureprop INNER JOIN cvterm AS prop ON (type_id=cvterm_id);
+  public.featureprop INNER JOIN public.cvterm AS prop ON (type_id=cvterm_id);
 
 -- association
-
-CREATE VIEW association AS
+--view
+CREATE VIEW godb.association AS
  SELECT
   feature_cvterm_id             AS id,
   cvterm_id                     AS term_id,
   feature_id                    AS gene_product_id,
-  is_not,
+  cast(NULL AS INT)             AS is_not,
+--FIXME THIS MAY BE IN THE SCHEMA NOW
+-- is_not,
 -- not used in godb; maybe never will be
   0                             AS role_group,
 -- lets be lazy for now; these are actually feature_cvtermprops
   0                             AS assocdate,
   0                             AS source_db_id
- FROM feature_cvterm;
+ FROM public.feature_cvterm;
 
 -- association_qualifier
-
-CREATE VIEW association_qualifier AS
+--view
+CREATE VIEW godb.association_qualifier AS
  SELECT
   feature_cvtermprop_id         AS id,
   feature_cvterm_id             AS association_id,
   type_id                       AS term_id,
   value                         AS value
- FROM feature_cvtermprop;
+ FROM public.feature_cvtermprop;
 
 -- evidence
 -- TODO!!
-
-CREATE VIEW evidence AS
+--view
+CREATE VIEW godb.evidence AS
  SELECT
   feature_cvtermprop_id         AS id,
   feature_cvterm_id             AS association_id,
   type_id                       AS term_id,
   value                         AS value
- FROM feature_cvtermprop
- INNER JOIN feature_cvterm_dbxref USING (feature_cvterm_id);
+ FROM public.feature_cvtermprop
+ INNER JOIN public.feature_cvterm_dbxref USING (feature_cvterm_id);
 
 -- evidence_dbxref
 -- TODO
-
-CREATE VIEW evidence_dbxref AS
+--view
+CREATE VIEW godb.evidence_dbxref AS
  SELECT
   feature_cvterm_id         AS id,
   feature_cvterm_id             AS association_id,
   type_id                       AS term_id,
   value                         AS value
- FROM feature_cvtermprop
- INNER JOIN feature_cvterm_dbxref USING (feature_cvterm_id);
+ FROM public.feature_cvtermprop
+ INNER JOIN public.feature_cvterm_dbxref USING (feature_cvterm_id);
 
 -- seq
-
-CREATE VIEW seq AS
+--view
+CREATE VIEW godb.seq AS
  SELECT
   feature_id                    AS id,
   feature.name                  AS display_id,
@@ -211,42 +304,42 @@ CREATE VIEW seq AS
   type.name                     AS moltype,
 -- this is never used
   0                             AS timestamp
- FROM feature INNER JOIN cvterm AS type ON (type_id=cvterm_id);
+ FROM public.feature INNER JOIN public.cvterm AS type ON (type_id=cvterm_id);
 
 -- seq_property [not used?]
-
-CREATE VIEW seq_property AS
+--view
+CREATE VIEW godb.seq_property AS
  SELECT
   *
- FROM feature WHERE NULL;
+ FROM public.feature WHERE NULL;
 
 -- seq_dbxref
 -- [in GO this typically has interpro ids, etc; diff semantics here]
-
-CREATE VIEW seq_dbxref AS
+--view
+CREATE VIEW godb.seq_dbxref AS
  SELECT 
   feature_id                    AS seq_id,
   dbxref_id
- FROM feature_dbxref;
+ FROM public.feature_dbxref;
 
 -- gene_product_seq
 -- 1:{0,1} relationship between features and seqs in chado
-
-CREATE VIEW gene_product_seq AS
+--view
+CREATE VIEW godb.gene_product_seq AS
  SELECT
   feature_id              AS gene_product_id,
   feature_id              AS seq_id
- FROM feature
+ FROM public.feature
  WHERE feature.residues IS NOT NULL;
 
 -- gene_product_count
 -- this is an OPTIONAL godb optimization
 --  simulate a rowless table for now
-
-CREATE VIEW gene_product_count AS
+--view
+CREATE VIEW godb.gene_product_count AS
  SELECT
   *
- FROM feature WHERE NULL;
+ FROM public.feature WHERE NULL;
 
 
 
