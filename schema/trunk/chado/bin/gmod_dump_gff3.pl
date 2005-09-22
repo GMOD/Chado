10fcdcb2f08e817c3d50ddfd364aeb25abc9eabf
scottcain <scottcain@224a875b-6a50-0410-9993-82261b5d0d45>
2005-09-22 20:45:52 +0000

    considerable improvement to the gff3 dumper:   -option to specify what conf file to use (ie, what database to dump from)   -option to include chado_feature_id as a attribute      (to allow linking between a gff database and chado databse)   -moved attribute string building out of the database and into perl,      removing a dependence on a postgres function (now it uses straight views only)

--- schema/trunk/chado/bin/gmod_dump_gff3.pl
+++ schema/trunk/chado/bin/gmod_dump_gff3.pl
@@ -5,6 +5,7 @@ use DBI;
 use Bio::GMOD::Config;
 use Bio::GMOD::DB::Config;
 use Getopt::Long;
+use URI::Escape;
 
 =head1 NAME
                                                                                 
@@ -22,6 +23,8 @@ are these:
 
   --organism          specifies the organism for the dump (common name)
   --refseq            reference sequece (eg, chromosome) to dump
+  --dbconf            the name of the conf file to use
+  --feature_ids       provide the chado feature_ids as attributes
 
 If there is no default organism for the database or one is not
 specified on the command line, the program will exit with no output.
@@ -38,17 +41,25 @@ it under the same terms as Perl itself.
 =cut
 
 
-my ($ORGANISM,$REFSEQ);
+my ($ORGANISM,$REFSEQ,$DBCONF,$FEATURE_IDS);
 
 GetOptions(
     'organism:s'  => \$ORGANISM,
     'refseq:s'    => \$REFSEQ,
+    'dbconf:s'    => \$DBCONF,
+    'feature_ids' => \$FEATURE_IDS,
 );
 
 my $gmod_conf = $ENV{'GMOD_ROOT'} ?
                 Bio::GMOD::Config->new($ENV{'GMOD_ROOT'}) :
                 Bio::GMOD::Config->new();
-my $db_conf = Bio::GMOD::DB::Config->new($gmod_conf,'default');
+
+my $db_conf;
+if ($DBCONF) {
+    $db_conf = Bio::GMOD::DB::Config->new($gmod_conf,$DBCONF);
+} else {
+    $db_conf = Bio::GMOD::DB::Config->new($gmod_conf,'default');
+}
 
 $ORGANISM ||=$db_conf->organism();
 
@@ -71,23 +82,27 @@ if ($REFSEQ) {
     $ref_seq_part = "and (name = '$REFSEQ' or ref = '$REFSEQ')"; 
 }
 
-my $sth = $dbh->prepare("select ref,source,type,fstart,fend,
-                                score,strand,phase,attributes,
-                                seqlen,name
+my $sth = $dbh->prepare("select feature_id,ref,source,type,fstart,fend,
+                                score,strand,phase, seqlen,name
                          from gff3view
                          where feature_id > ? and feature_id < ? and
                          organism_id = ? $ref_seq_part
-                         order by feature_id");
+                         order by feature_id") or die $!;
+
+my $attr_query =  $dbh->prepare("select feature_id, type, attribute
+                         from gff3atts
+                         where feature_id > ? and feature_id < ?
+                         order by feature_id, type") or die $!;
 
 my $max_feature_id = $dbh->prepare("select max(feature_id) from feature
-                                    where organism_id = ? $ref_seq_part");
+                                    where organism_id = ?") or die;
 $max_feature_id->execute($organism_id);
 
 $arrayref       = $max_feature_id->fetchrow_arrayref;
 $max_feature_id = $$arrayref[0];
 
 my $min_feature_id = $dbh->prepare("select min(feature_id) from feature
-                                    where organism_id = ? $ref_seq_part" );
+                                    where organism_id = ?" ) or die;
 $min_feature_id->execute($organism_id);
 
 $arrayref       = $min_feature_id->fetchrow_arrayref;
@@ -99,7 +114,12 @@ my @ref_seqs;
 print "##gff-version   3\n";
 for (my $i = $min_feature_id; $i<=$max_feature_id;$i = $i + $CHUNK) {
     my $upper = $i+$CHUNK+1;
-    $sth->execute($i,$upper,$organism_id);
+
+    $attr_query->execute($i,$upper,) or die $!;
+
+    my $attstr_hashref = make_attstr_hashref($attr_query);
+
+    $sth->execute($i,$upper,$organism_id) or die $!;
 
     while (my $hashref = $sth->fetchrow_hashref) {
         my $ref    = $$hashref{ref};
@@ -114,7 +134,8 @@ for (my $i = $min_feature_id; $i<=$max_feature_id;$i = $i + $CHUNK) {
             $strand = '.';
         }
         my $phase  = $$hashref{phase}      || '.';
-        my $atts   = $$hashref{attributes} || '.';
+        my $atts   = $$attstr_hashref{$$hashref{feature_id}}
+                                           || '.';
         
         unless ($ref) { #must be a reference sequence
             $ref   = $$hashref{name};
@@ -137,6 +158,7 @@ for (my $i = $min_feature_id; $i<=$max_feature_id;$i = $i + $CHUNK) {
 
 $sth = $dbh->prepare("select residues from feature 
                       where name=? and residues is not null");
+print "##FASTA\n";
 foreach my $ref (@ref_seqs) {
     $sth->execute($ref);
     while (my $data = $sth->fetchrow_arrayref) {
@@ -158,3 +180,30 @@ foreach my $ref (@ref_seqs) {
 }
 
 
+sub make_attstr_hashref {
+    my $query_handle = shift;
+
+    my $old_id   = 0;
+    my $old_type = '';
+    my %attstr_hash;
+    while (my $hashref = $query_handle->fetchrow_hashref) {
+        my $escaped_att = uri_escape($$hashref{attribute}, "^a-zA-Z0-9. :^*$@!+_?-");
+        next if ($$hashref{type} eq 'chado_feature_id' and !$FEATURE_IDS);
+        if ($old_id    eq $$hashref{feature_id} and
+            $old_type  eq $$hashref{type} ) {
+
+            $attstr_hash{$old_id} .= ",$escaped_att"
+        }
+        elsif ($old_id eq $$hashref{feature_id}) {
+            $old_type =$$hashref{type};
+            $attstr_hash{$old_id} .= ";$old_type=$escaped_att";
+        } 
+        else {
+            $old_type= $$hashref{type};
+            $old_id  = $$hashref{feature_id};
+            $attstr_hash{$old_id} = "$old_type=$escaped_att";
+        }
+    }
+
+    return \%attstr_hash;
+} 
