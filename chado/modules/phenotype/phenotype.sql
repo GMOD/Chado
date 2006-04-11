ede41841ffaaa4d4af6c0d06ee9dac2a13c38e6a
cmungall <cjmungall@lbl.gov>
2006-04-11 14:47:18 +0000

    split genetic module - pheno tables in new module

---
+++ chado/modules/phenotype/phenotype.sql
@@ -0,0 +1,58 @@
+
+CREATE TABLE phenotype (
+    phenotype_id SERIAL NOT NULL,
+    primary key (phenotype_id),
+    uniquename TEXT NOT NULL,  
+    observable_id INT,
+    FOREIGN KEY (observable_id) REFERENCES cvterm (cvterm_id) ON DELETE CASCADE,
+    attr_id INT,
+    FOREIGN KEY (attr_id) REFERENCES cvterm (cvterm_id) ON DELETE SET NULL,
+    value TEXT,
+    cvalue_id INT,
+    FOREIGN KEY (cvalue_id) REFERENCES cvterm (cvterm_id) ON DELETE SET NULL,
+    assay_id INT,
+    FOREIGN KEY (assay_id) REFERENCES cvterm (cvterm_id) ON DELETE SET NULL,
+    CONSTRAINT phenotype_c1 UNIQUE (uniquename)
+);
+CREATE INDEX phenotype_idx1 ON phenotype (cvalue_id);
+CREATE INDEX phenotype_idx2 ON phenotype (observable_id);
+CREATE INDEX phenotype_idx3 ON phenotype (attr_id);
+
+COMMENT ON TABLE phenotype IS 'a phenotypic statement, or a single atomic phenotypic observation a controlled sentence describing observable effect of non-wt function -- e.g. Obs=eye, attribute=color, cvalue=red';
+
+COMMENT ON COLUMN phenotype.observable_id IS 'The entity: e.g. anatomy_part, biological_process';
+COMMENT ON COLUMN phenotype.attr_id IS 'Phenotypic attribute (quality, property, attribute, character) - drawn from PATO';
+COMMENT ON COLUMN phenotype.value IS 'value of attribute - unconstrained free text. Used only if cvalue_id is not appropriate';
+COMMENT ON COLUMN phenotype.cvalue_id IS 'Phenotype attribute value (state)';
+COMMENT ON COLUMN phenotype.assay_id IS 'evidence type';
+
+CREATE TABLE phenotype_cvterm (
+    phenotype_cvterm_id SERIAL NOT NULL,
+    primary key (phenotype_cvterm_id),
+    phenotype_id INT NOT NULL,
+    FOREIGN KEY (phenotype_id) REFERENCES phenotype (phenotype_id) ON DELETE CASCADE,
+    cvterm_id INT NOT NULL,
+    FOREIGN KEY (cvterm_id) REFERENCES cvterm (cvterm_id) ON DELETE CASCADE,
+    CONSTRAINT phenotype_cvterm_c1 UNIQUE (phenotype_id, cvterm_id)
+);
+CREATE INDEX phenotype_cvterm_idx1 ON phenotype_cvterm (phenotype_id);
+CREATE INDEX phenotype_cvterm_idx2 ON phenotype_cvterm (cvterm_id);
+
+COMMENT ON TABLE phenotype_cvterm IS NULL;
+
+ 
+CREATE TABLE feature_phenotype (
+    feature_phenotype_id SERIAL NOT NULL,
+    primary key (feature_phenotype_id),
+    feature_id INT NOT NULL,
+    FOREIGN KEY (feature_id) REFERENCES feature (feature_id) ON DELETE CASCADE,
+    phenotype_id INT NOT NULL,
+    FOREIGN KEY (phenotype_id) REFERENCES phenotype (phenotype_id) ON DELETE CASCADE,
+    CONSTRAINT feature_phenotype_c1 UNIQUE (feature_id,phenotype_id)       
+);
+CREATE INDEX feature_phenotype_idx1 ON feature_phenotype (feature_id);
+CREATE INDEX feature_phenotype_idx2 ON feature_phenotype (phenotype_id);
+
+COMMENT ON TABLE feature_phenotype IS NULL;
+
+
