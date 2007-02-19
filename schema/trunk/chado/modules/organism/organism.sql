c39d7e8f8acdbb7e993b59c7e7b7aae55c053093
briano <briano@224a875b-6a50-0410-9993-82261b5d0d45>
2007-02-19 20:15:44 +0000

    Minor edits, add dependencies, add Id line

--- schema/trunk/chado/modules/organism/organism.sql
+++ schema/trunk/chado/modules/organism/organism.sql
@@ -1,3 +1,17 @@
+-- $Id: organism.sql,v 1.17 2007-02-19 20:15:44 briano Exp $
+-- ==========================================
+-- Chado organism module
+
+-- ============
+-- DEPENDENCIES
+-- ============
+-- :import cvterm from cv
+-- :import dbxref from general
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+-- ================================================
+-- TABLE: organism
+-- ================================================
 
 create table organism (
 	organism_id serial not null,
@@ -7,7 +21,7 @@ create table organism (
 	species varchar(255) not null,
 	common_name varchar(255) null,
 	comment text null,
-    constraint organism_c1 unique (genus,species)
+	constraint organism_c1 unique (genus,species)
 );
 
 COMMENT ON TABLE organism IS 'The organismal taxonomic
@@ -16,12 +30,16 @@ phylogeny module, and taxonomies can be represented using the cvterm
 module or the phylogeny module';
 
 COMMENT ON COLUMN organism.species IS 'A type of organism is always
-uniquely identified by genus+species. When mapping from the NCBI
+uniquely identified by genus and species. When mapping from the NCBI
 taxonomy names.dmp file, the unique-name column must be used where it
 is present, as the name column is not always unique (eg environmental
 samples). If a particular strain or subspecies is to be represented,
 this is appended onto the species name. Follows standard NCBI taxonomy
 pattern';
+
+-- ================================================
+-- TABLE: organism_dbxref
+-- ================================================
 
 create table organism_dbxref (
     organism_dbxref_id serial not null,
@@ -34,6 +52,10 @@ create table organism_dbxref (
 );
 create index organism_dbxref_idx1 on organism_dbxref (organism_id);
 create index organism_dbxref_idx2 on organism_dbxref (dbxref_id);
+
+-- ================================================
+-- TABLE: organismprop
+-- ================================================
 
 create table organismprop (
     organismprop_id serial not null,
@@ -49,7 +71,7 @@ create table organismprop (
 create index organismprop_idx1 on organismprop (organism_id);
 create index organismprop_idx2 on organismprop (type_id);
 
-COMMENT ON TABLE organismprop IS 'tag-value properties - follows standard chado model';
+COMMENT ON TABLE organismprop IS 'Tag-value properties - follows standard chado model.';
 
 -- ================================================
 -- TABLE: organism_relationship
