7390436b7e509e8f44466252d5fe00a5214e13f7
Brian O. <briano@users.sourceforge.net>
2007-02-20 17:24:04 +0000

    Tables no longer in distribution

--- chado/bin/Skip_tables.pm
+++ chado/bin/Skip_tables.pm
@@ -8,23 +8,9 @@ require Exporter;
 @ISA       = qw(Exporter);
 @EXPORT_OK = qw(@skip_tables);
 
-@skip_tables = ('affymetrixprobeset',
-                'affymetrixprobe',
-                'affymetrixcel',
-                'affymetrixsnp',
-                'affymetrixmas5',
-                'affymetrixdchip',
-                'affymetrixvsn',
-                'affymetrixsea',
-                'affymetrixplier',
-                'affymetrixdabg',
-                'affymetrixrma',
-                'affymetrixgcrma',
-                'affymetrixprobesetstat',
-                'gencode_codon_aa',
+@skip_tables = ('gencode_codon_aa',
                 'gencode_startcodon',
                 'gencode');
-
 
 
 1;
