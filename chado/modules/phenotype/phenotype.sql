c4749d35080a1765319ac76d1edbde6aaeb0957b
Brian O. <briano@users.sourceforge.net>
2007-03-01 04:15:26 +0000

    Minor edits

--- chado/modules/phenotype/phenotype.sql
+++ chado/modules/phenotype/phenotype.sql
@@ -1,4 +1,4 @@
--- $Id: phenotype.sql,v 1.4 2007-02-20 19:45:06 briano Exp $
+-- $Id: phenotype.sql,v 1.5 2007-03-01 04:15:26 briano Exp $
 -- ==========================================
 -- Chado phenotype module
 --
@@ -34,13 +34,13 @@ CREATE INDEX phenotype_idx2 ON phenotype (observable_id);
 CREATE INDEX phenotype_idx3 ON phenotype (attr_id);
 
 COMMENT ON TABLE phenotype IS 'A phenotypic statement, or a single
-atomic phenotypic observation is a controlled sentence describing
-observable effects of non-wild type function E.g. Obs=eye, attribute=color, cvalue=red';
+atomic phenotypic observation, is a controlled sentence describing
+observable effects of non-wild type function. E.g. Obs=eye, attribute=color, cvalue=red.';
 COMMENT ON COLUMN phenotype.observable_id IS 'The entity: e.g. anatomy_part, biological_process.';
 COMMENT ON COLUMN phenotype.attr_id IS 'Phenotypic attribute (quality, property, attribute, character) - drawn from PATO.';
 COMMENT ON COLUMN phenotype.value IS 'Value of attribute - unconstrained free text. Used only if cvalue_id is not appropriate.';
 COMMENT ON COLUMN phenotype.cvalue_id IS 'Phenotype attribute value (state).';
-COMMENT ON COLUMN phenotype.assay_id IS 'Evidence type';
+COMMENT ON COLUMN phenotype.assay_id IS 'Evidence type.';
 
 -- ================================================
 -- TABLE: phenotype_cvterm
