4dd23d1b86fc324601df570c90c6f5756e068d8a
Brian O. <briano@users.sourceforge.net>
2007-04-01 18:45:41 +0000

    Remove organismpath and organism_relationship

--- chado/modules/organism/organism.sql
+++ chado/modules/organism/organism.sql
@@ -1,7 +1,7 @@
--- $Id: organism.sql,v 1.18 2007-03-01 03:57:56 briano Exp $
+-- $Id: organism.sql,v 1.19 2007-04-01 18:45:41 briano Exp $
 -- ==========================================
 -- Chado organism module
-
+--
 -- ============
 -- DEPENDENCIES
 -- ============
@@ -72,43 +72,4 @@ create index organismprop_idx1 on organismprop (organism_id);
 create index organismprop_idx2 on organismprop (type_id);
 
 COMMENT ON TABLE organismprop IS 'Tag-value properties - follows standard chado model.';
-
--- ================================================
--- TABLE: organism_relationship
--- ================================================
-
-CREATE TABLE organism_relationship (
-    organism_relationship_id serial not null,
-    PRIMARY KEY (organism_relationship_id),
-    subject_id int not null,
-    FOREIGN KEY (subject_id) REFERENCES organism (organism_id) INITIALLY DEFERRED,
-    object_id int not null,
-    FOREIGN KEY (object_id) REFERENCES organism (organism_id) INITIALLY DEFERRED,
-    type_id int not null,
-    FOREIGN KEY (type_id) REFERENCES cvterm (cvterm_id) INITIALLY DEFERRED,
-    CONSTRAINT organism_relationship_c1 UNIQUE (subject_id, object_id, type_id)
-);
-CREATE INDEX organism_relationship_idx1 ON organism_relationship (subject_id);
-CREATE INDEX organism_relationship_idx2 ON organism_relationship (object_id);
-CREATE INDEX organism_relationship_idx3 ON organism_relationship (type_id);
-
--- ================================================
--- TABLE: organismpath
--- ================================================
-
-CREATE TABLE organismpath (
-    organismpath_id serial not null,
-    PRIMARY KEY (organismpath_id),
-    subject_id int not null,
-    FOREIGN KEY (subject_id) REFERENCES organism (organism_id) INITIALLY DEFERRED,
-    object_id int not null,
-    FOREIGN KEY (object_id) REFERENCES organism (organism_id) INITIALLY DEFERRED,
-    type_id int not null,
-    FOREIGN KEY (type_id) REFERENCES cvterm (cvterm_id) INITIALLY DEFERRED,
-    pathdistance int,
-    CONSTRAINT organismpath_c1 UNIQUE (subject_id,object_id,type_id,pathdistance)
-);
-CREATE INDEX organismpath_idx1 ON organismpath (type_id);
-CREATE INDEX organismpath_idx2 ON organismpath (subject_id);
-CREATE INDEX organismpath_idx3 ON organismpath (object_id);
 
