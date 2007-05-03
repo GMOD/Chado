bb9021b4bbc8b6631b6a1014e91b064808cda737
scottcain <scottcain@224a875b-6a50-0410-9993-82261b5d0d45>
2007-05-03 20:31:03 +0000

    making Makefile.PL a little happier when ant isn't installed and cleaning up the dbxref to gff utility

--- schema/trunk/chado/bin/gmod_make_gff_from_dbxref.pl
+++ schema/trunk/chado/bin/gmod_make_gff_from_dbxref.pl
@@ -1,28 +1,31 @@
-#!/usr/bin/perl
+#!/usr/bin/perl 
 use strict;
 use warnings;
 
 use Bio::DB::Fasta;
-use File::Temp qw/ tempdir /;
+use File::Temp qw/ tempdir tempfile /;
 use Getopt::Long;
 use File::Copy;
 use File::Spec::Functions;
 
-my ( $FASTA_DIR );
+my ( $FASTA_DIR, $TMP_DIR );
 
 GetOptions (
-    "fasta_dir"  => \$FASTA_DIR,
+    "fasta_dir:s"  => \$FASTA_DIR,
+    "tmp_dir:s"    => \$TMP_DIR,
 );
+
+$TMP_DIR ||= "/tmp";
 
 my %dbxref;
 while(<>) {
     chomp;
-    my ($db, $acc) = split /\t/;
+    my ($db, $acc) = split /\s+/;
     $dbxref{$db}{$acc} = 1;
 }
 
-my $tmp_dir = tempdir( CLEANUP => 1);
-my $tmp_fasta = tempfile();
+my $tmp_dir = tempdir( CLEANUP => 1, DIR => $TMP_DIR );
+my $tmp_fasta = tempfile( DIR => $tmp_dir );
 warn "created tempdir $tmp_dir\n";
 
 opendir(DIR, $FASTA_DIR) or die "couldn't open $FASTA_DIR: $!";
@@ -31,26 +34,30 @@ closedir(DIR);
 
 my @fasta_save;
 for my $fasta_file (@fasta_files) {
-    copy($fasta_file, $tmp_dir)
+    next if $fasta_file =~ /^\./;
+    $fasta_file = catfile( $FASTA_DIR, $fasta_file);
+    warn "Copying $fasta_file to the temp directory\n";
+    copy($fasta_file, $tmp_dir) 
         or die "couldn't move $fasta_file to $tmp_dir (a temp dir): $!";    
 
-    my $db = Bio::DB::Fasta->new($tmp_dir,
-                                 -makeid  => \&make_id,
-                                 -reindex => 1,);
+    my $fasta_db = Bio::DB::Fasta->new($tmp_dir,
+                                       -makeid  => \&make_id,
+                                       -reindex => 1,);
 
     for my $db (keys %dbxref) {
         for my $acc (keys %{$dbxref{$db}}) {
-            my $seq = $db->seq($acc);
+            my $seq = $fasta_db->seq($acc);
             if ($seq) {
                 my $length = length $seq;
                 my $ninth  = "ID=$acc;Name=$acc;Dbxref=$db:$acc";
-                print join("\t",$acc,".","region",1,$length,".",".",".",$ninth);
-                print $tmp_fasta, ">$acc\n$seq\n";
+                print join("\t",$acc,".","region",1,$length,".",".",".",$ninth),"\n";
+                $tmp_fasta->print( ">$acc\n$seq\n" );
                 delete $dbxref{$db}{$acc};
             }
         }
     }
-    my (undef, undef, $filename) = splitpath($fasta_file);
+    my (undef, undef, $filename) = File::Spec->splitpath($fasta_file);
+    warn "Finished processing $filename\n";
     unlink "$tmp_dir/$filename";
 }
 print "##FASTA\n";
