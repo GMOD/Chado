f8a90b41d0d10e290b83eb53cacb7d9b961c25bf
e-just <e-just@224a875b-6a50-0410-9993-82261b5d0d45>
2007-08-24 21:28:51 +0000

    added relations to box and classes for row and row_metadata

--- schema/trunk/hackathon_2007/wikitrips/Wiki/DBI.pm
+++ schema/trunk/hackathon_2007/wikitrips/Wiki/DBI.pm
@@ -7,3 +7,13 @@ __PACKAGE__->set_db('Main', 'dbi:mysql:wikibox_db', 'root' );
 package Wiki::Box;
 use base 'Wiki::DBI';
 __PACKAGE__->set_up_table("box");
+Wiki::Box->has_many( rows =>Wiki::Row=>'box_id');
+
+package Wiki::Row;
+use base 'Wiki::DBI';
+__PACKAGE__->set_up_table("row");
+Wiki::Box->has_many( row_metadata =>Wiki::RowMetadata=>'row_id');
+
+package Wiki::RowMetadata;
+use base 'Wiki::DBI';
+__PACKAGE__->set_up_table("row_metadata");
