32a714e84e8775f17abce0bde52729deda3db32c
e-just <e-just@224a875b-6a50-0410-9993-82261b5d0d45>
2007-08-24 22:52:50 +0000

    now takes arguments for gene name and template and outputs template

--- schema/trunk/hackathon_2007/wikitrips/create_text_file_modware.pl
+++ schema/trunk/hackathon_2007/wikitrips/create_text_file_modware.pl
@@ -1,14 +1,15 @@
 #!/usr/bin/perl
-#
+
+my ( $gene_name_input, $template ) = @ARGV;
+
+die "please pass gene_name and template_name as arguments\n" if ( !$gene_name_input || !$template);
 
 use Modware::Search::Gene;
 
-my ($gene) = Modware::Search::Gene->Search_by_name( 'sadA');
+my ($gene) = Modware::Search::Gene->Search_by_name( $gene_name_input );
 
 my $gene_name   = $gene->name();
 my $description = $gene->description();
 my @synonyms    = @{$gene->synonyms()};
 my $syn_string  = join ", ", @synonyms;
-
-print $gene_name."\t".$gene_name.'||'.$description.'||'.$syn_string."\n";
-;
+print $gene_name."\t".$template."\t".$gene_name.'||'.$description.'||'.$syn_string."\n";
