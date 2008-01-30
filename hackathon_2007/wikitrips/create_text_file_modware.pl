9195dd567eb039c88d14c10870817033c27c9bc4
Scott Cain <scott@scottcain.net>
2008-01-30 15:28:02 +0000

    updating the out of chado towards wiki script--now handles wildcards and multiple rows

--- hackathon_2007/wikitrips/create_text_file_modware.pl
+++ hackathon_2007/wikitrips/create_text_file_modware.pl
@@ -1,15 +1,20 @@
 #!/usr/bin/perl
 use strict;
+use warnings;
+
 my ( $gene_name_input, $page_template, $table_template ) = @ARGV;
 
 die "please pass gene_name, page_template, table_template as arguments\n" if ( !$gene_name_input || !$page_template || !$table_template );
 
 use Modware::Search::Gene;
 
-my ($gene) = Modware::Search::Gene->Search_by_name( $gene_name_input );
+my $genes = Modware::Search::Gene->Search_by_name( $gene_name_input );
 
-my $gene_name   = $gene->name();
-my $description = $gene->description();
-my @synonyms    = @{$gene->synonyms()};
-my $syn_string  = join ", ", @synonyms;
-print $gene_name."\t".$page_template."\t".$table_template."\t".$gene_name.'||'.$description.'||'.$syn_string."\n";
+while ( my $gene = $genes->next ) {
+    my $gene_name   = $gene->name();
+    my $description = $gene->description() || '';
+    my @synonyms    = @{$gene->synonyms()};
+    my $syn_string  = join ", ", @synonyms || '';
+    my $row_data    = join ("||", $gene_name, $description, $syn_string);
+    print join("\t",$gene_name,$page_template,$table_template,$row_data),"\n";
+}
