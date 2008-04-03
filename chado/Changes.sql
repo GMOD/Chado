81e996168e1ec6c74700cfd7be701102581f2b66
Scott Cain <scott@scottcain.net>
2008-04-03 21:49:41 +0000

    bookkeeping getting ready for release 1.01

---
+++ chado/Changes.sql
@@ -0,0 +1,6 @@
+--when schema elements are added, including new tables or changes to table
+--columns, or when initialize.sql is changed, the sql to make those changes
+--happen should go here.
+
+--gmod version 1.01
+
