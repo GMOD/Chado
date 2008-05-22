0aec71fb950e24a0bc16001b6c778a720fe9a485
cmungall <cjmungall@lbl.gov>
2008-05-22 21:52:00 +0000

    *** empty log message ***

--- chado/modules/sequence/views/implicit-feature-views.sql
+++ chado/modules/sequence/views/implicit-feature-views.sql
@@ -1,3 +1,18 @@
+-- DEPENDENCY:
+--  chado/modules/bridges/sofa-bridge.sql
+
+-- The standard Chado pattern for protein coding genes
+-- is a feature of type 'gene' with 'mRNA' features as parts
+-- REQUIRES: 'mrna' view from sofa-bridge.sql
+CREATE OR REPLACE VIEW protein_coding_gene AS
+ SELECT
+  DISTINCT gene.*
+ FROM
+  feature AS gene
+  INNER JOIN feature_relationship AS fr ON (gene.feature_id=fr.object_id)
+  INNER JOIN mrna ON (mrna.feature_id=fr.subject_id);
+
+
 -- introns are implicit from surrounding exons
 -- combines intron features with location and parent transcript
 -- the same intron appearing in multiple transcripts will appear
