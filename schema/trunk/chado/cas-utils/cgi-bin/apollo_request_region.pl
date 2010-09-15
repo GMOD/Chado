e55d0a416a7a93aaec2b8d29a09183edf59ff5d1
scottcain <scottcain@224a875b-6a50-0410-9993-82261b5d0d45>
2010-09-15 20:27:43 +0000

    Modified apollo_request_region.pl to optionally return a jnlp that allows direct database access.

--- schema/trunk/chado/cas-utils/cgi-bin/apollo_request_region.pl
+++ schema/trunk/chado/cas-utils/cgi-bin/apollo_request_region.pl
@@ -13,13 +13,14 @@ my $hostname    = $config->PROJECT_HOME;
 my $web_path    = $config->WEB_PATH;
 my $vendor      = $config->VENDOR;
 my $apollo_desc = $config->APOLLO_DESC;
+my $access_method = $config->ACCESS_METHOD;
 
 unless(param()) { #print the starting page
 
     print header,
-         start_html(-title=>'Script for generating game on the fly from apollo',
+         start_html(-title=>'Script for generating a connection on the fly for apollo',
                     -style=>{src=>'/gbrowse/gbrowse.css'},),
-         h1('Request a GAME-XML file from the database'),
+         h1('Request a region from the database'),
          start_form(-method=>'GET'),
          'Chromosome:',
          popup_menu(-name=>'chromosome',
@@ -34,6 +35,13 @@ unless(param()) { #print the starting page
              -default=>'game',
              -labels=>{'GAME-XML','chadoxml'}
          ),p,
+         'or ',
+         checkbox(
+             -name=>'direct',
+             -checked=> $access_method eq 'direct' ? 1 : 0,
+             -value=>'ON',
+             -label=>'Direct database access',
+         ),
          submit,
          end_form,
          end_html;
@@ -47,6 +55,7 @@ my $t_start      = param('start');
 my $t_end        = param('end');
 my $t_selection  = param('selection');
 my $t_xmltype    = param('xml_type');
+my $t_direct     = param('direct');
 
 if ($t_chromosome and $t_chromosome =~ /^(\d+)$/) {
     $chromosome = $1; 
@@ -71,10 +80,7 @@ if ($t_selection  and $t_selection  =~ /^(\w+):(\d+)\.\.(\d+)$/) {
 if ($t_xmltype and ($t_xmltype =~ /^chado$/ or $t_xmltype =~ /^game$/)) {
     $xmltype = $t_xmltype;
 }
-else {
-    $xmltype = 'game';
-}
-    
+ 
 unless ($chromosome && $start && $end) {
     $failure = p("The parameters used for this script weren't understood; here's what I got:")
               .start_ul  . li("chromosome = $chromosome")
@@ -86,7 +92,8 @@ unless ($chromosome && $start && $end) {
 
 my $filename = "$chromosome:$start\-$end";
 
-my $jscript = <<END;
+if ($t_xmltype and !$t_direct) {
+  my $jscript = <<END;
 function visibility(id1,id2) {
   var visible = YAHOO.util.Dom.getStyle (id1,'display') == 'none' ? ['inline','none'] : ['none','inline'];
   YAHOO.util.Dom.setStyle(id1,'display',visible[0]);
@@ -94,13 +101,13 @@ function visibility(id1,id2) {
 }
 END
 
-my $style = <<END;
+  my $style = <<END;
 .showhide { position:relative; top:0px;}
 .output { border:1px solid blue; width:85%; overflow:auto; }
 .control { cursor:pointer }
 END
 
-print header,
+  print header,
       start_html(-title=>"Download $filename",
                  -style=>[{src=>'/gbrowse/gbrowse.css'},
                           {code=>$style}],
@@ -112,51 +119,51 @@ print header,
       p('This may take a while...'),
       start_div(-class=>"output");
 
-my $javacmd = "$apollo -w $working_dir/$filename.xml -o $xmltype -l $filename -i chadoDB";
+  my $javacmd = "$apollo -w $working_dir/$filename.xml -o $xmltype -l $filename -i chadoDB";
 
-print start_div({-id=>'invisible',
+  print start_div({-id=>'invisible',
                  -class=>'showhide',
                  -style=>"display:none"}),
       start_span({-onclick=>"visibility('visible','invisible')"}),
       "[-] Hide Apollo output<br>",
       end_span,
       start_pre;
-system($javacmd);
-print end_pre,end_div();
-print start_div({-id=>'visible',
+  system($javacmd);
+  print end_pre,end_div();
+  print start_div({-id=>'visible',
                  -class=>'showhide',}),
       start_span({-onclick=>"visibility('visible','invisible')"}),
       "[+] Show Apollo output<br>",
       end_span(),end_div(),end_div(),"<br clear=all>";
 
-print start_span(-class=>'position:relative');
+  print start_span(-class=>'position:relative');
 
-my $error_flag;
-if (-e "$working_dir/$filename.xml") {
+  my $error_flag;
+  if (-e "$working_dir/$filename.xml") {
     print p("The file $filename.xml has been created");
-}
-else {
+  }
+  else {
     print h3("The file $filename.xml was not created; check the Apollo output for errors");
     $error_flag = 1;
-}
+  }
 
-open OUT, ">$working_dir/$filename.jnlp" 
+  open OUT, ">$working_dir/$filename.jnlp" 
    or handle_error(
         p("I couldn't open file $working_dir/$filename.jnlp for writing: $!"));
 
-print OUT write_jnlp();
+  print OUT write_jnlp();
 
-close OUT;
+  close OUT;
 
-if (-e "$working_dir/$filename.jnlp") {
+  if (-e "$working_dir/$filename.jnlp") {
     print p("Created the file $filename.jnlp.");
-}
-else {
+  }
+  else {
     print h3("The file $filename.jnlp was not created; I don't know why");
     $error_flag = 1;
-}
+  }
    
-if (!$error_flag) { 
+  if (!$error_flag) { 
     print  p("Click on this link: "
       . a({href=>"$hostname$web_path/$filename.jnlp"},
           "$filename.jnlp")
@@ -164,9 +171,17 @@ if (!$error_flag) {
       . a({href=>"$hostname/cgi-bin/upload_game.pl"},
           "Upload game-xml")
       ." link to upload your annotations."),
-}
+  }
 
 print  end_span, end_html; 
+} #end if to give html page to click on xml based jnlp link
+elsif ($t_direct) {
+  my $file_contents = write_jnlp($t_direct);
+
+  print header(-type => 'application/x-java-jnlp-file'),
+        $file_contents ;
+}
+
 
 exit(0);
 
@@ -177,8 +192,9 @@ sub handle_error {
 }
 
 sub write_jnlp {
+    my $direct = shift;
 
-    return <<END;
+    my $file =<<END
 <?xml version="1.0" encoding="UTF-8"?>
 <jnlp codebase="$hostname/apollo/webstart" 
 href="$hostname$web_path/$filename.jnlp" spec="1.0+">
@@ -231,14 +247,36 @@ href="$hostname$web_path/$filename.jnlp" spec="1.0+">
     <!-- we can add arguments when launching Apollo - this particular one allows us to
               load chromosome 1, from 11650000 to 11685000 - great way to have Apollo load
               specific regions -->
+
+END
+;
+
+  if ($direct) {
+    $file .=<<A_EXTRA
+    <argument>-i</argument>
+    <argument>chadodb</argument>
+    <argument>-l</argument>
+    <argument>$filename</argument>
+A_EXTRA
+; 
+  }
+  else {
+    $file .=<<B_EXTRA
     <argument>-i</argument>
     <argument>game</argument>
     <argument>-f</argument>
     <argument>$hostname$web_path/$filename.xml</argument>
+B_EXTRA
+;
+  }
+
+  $file .=<<FINAL
   </application-desc>
 </jnlp>
-END
+FINAL
 ;
+
+  return $file;
 }
 
 =pod
