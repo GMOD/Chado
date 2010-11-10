cfa27025cc3e11004fdd633854c1188075213643
scottcain <scottcain@224a875b-6a50-0410-9993-82261b5d0d45>
2010-11-10 15:43:27 +0000

    fixes to improve the GFF3-ness of gff3 dumps from gbrowse

--- schema/trunk/chado/modules/sequence/gff-bridge/sequence-gff-views.sql
+++ schema/trunk/chado/modules/sequence/gff-bridge/sequence-gff-views.sql
@@ -6,30 +6,15 @@ CREATE OR REPLACE VIEW gffatts (
     type,
     attribute
 ) AS
-SELECT feature_id, 'cvterm' AS type,  s.name AS attribute
+SELECT feature_id, 'Ontology_term' AS type,  s.name AS attribute
 FROM cvterm s, feature_cvterm fs
 WHERE fs.cvterm_id = s.cvterm_id
 UNION ALL
-SELECT feature_id, 'dbxref' AS type, d.name || ':' || s.accession AS attribute
+SELECT feature_id, 'Dbxref' AS type, d.name || ':' || s.accession AS attribute
 FROM dbxref s, feature_dbxref fs, db d
 WHERE fs.dbxref_id = s.dbxref_id and s.db_id = d.db_id
---SELECT feature_id, 'expression' AS type, s.description AS attribute
---FROM expression s, feature_expression fs
---WHERE fs.expression_id = s.expression_id
---UNION ALL
---SELECT fg.feature_id, 'genotype' AS type, g.uniquename||': '||g.description AS attribute
---FROM gcontext g, feature_gcontext fg
---WHERE g.gcontext_id = fg.gcontext_id
---UNION ALL
---SELECT feature_id, 'genotype' AS type, s.description AS attribute
---FROM genotype s, feature_genotype fs
---WHERE fs.genotype_id = s.genotype_id
---UNION ALL
---SELECT feature_id, 'phenotype' AS type, s.description AS attribute
---FROM phenotype s, feature_phenotype fs
---WHERE fs.phenotype_id = s.phenotype_id
 UNION ALL
-SELECT feature_id, 'synonym' AS type, s.name AS attribute
+SELECT feature_id, 'Alias' AS type, s.name AS attribute
 FROM synonym s, feature_synonym fs
 WHERE fs.synonym_id = s.synonym_id
 UNION ALL
@@ -115,13 +100,15 @@ feature_id, ref, source, type, fstart, fend,
 score, strand, phase, seqlen, name, organism_id
 ) AS
 SELECT
-f.feature_id, sf.name, gffdbx.accession, cv.name,
-fl.fmin+1, fl.fmax, af.significance,
+f.feature_id, sf.name, 
+ COALESCE(gffdbx.accession,'.'::varchar(255)), cv.name,
+fl.fmin+1, fl.fmax, 
+ COALESCE(CAST(af.significance AS text), '.'),
  CASE WHEN fl.strand=-1 THEN '-'
       WHEN fl.strand=1  THEN '+'
       ELSE '.'
  END,
-fl.phase, f.seqlen, f.name, f.organism_id
+ COALESCE(CAST(fl.phase AS text), '.'), f.seqlen, f.name, f.organism_id
 FROM feature f
 LEFT JOIN featureloc fl ON (f.feature_id = fl.feature_id)
 LEFT JOIN feature sf ON (fl.srcfeature_id = sf.feature_id)
