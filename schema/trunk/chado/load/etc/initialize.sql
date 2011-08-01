36e21512ae239cdd1b56b03374331477cc22763e
scottcain <scottcain@224a875b-6a50-0410-9993-82261b5d0d45>
2011-08-01 18:50:10 +0000

    adding init code to lay the foundation for schema version data

--- schema/trunk/chado/load/etc/initialize.sql
+++ schema/trunk/chado/load/etc/initialize.sql
@@ -214,6 +214,11 @@ insert into cvterm (name,definition,cv_id,dbxref_id) values ('histogram','sensu 
 
 insert into cv (name,definition) values ('autocreated','Terms that are automatically inserted by loading software');
 
+insert into cv (name,definition) values ('chado_properties','Terms that are used in the chadoprop table to describe the state of the database');
+
+insert into dbxref (db_id,accession) values ((select db_id from db where name='null'), 'chado_properties:version');
+insert into cvterm (name,definition,cv_id,dbxref_id) values ('version','Chado schema version',(select cv_id from cv where name = 'chado_properties'),(select dbxref_id from dbxref where accession='chado_properties:version'));
+
 
 --this table will probably end up in general.sql
  CREATE TABLE public.materialized_view   (       
