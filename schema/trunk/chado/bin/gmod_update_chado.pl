326859c573e198a8e05d7ba94096290e4288b978
scottcain <scottcain@224a875b-6a50-0410-9993-82261b5d0d45>
2011-08-31 20:17:50 +0000

    multiple changes to make the schema updater work

---
+++ schema/trunk/chado/bin/gmod_update_chado.pl
@@ -0,0 +1,76 @@
+#!/usr/bin/perl -w
+use strict;
+
+use Getopt::Long;
+use lib '/home/cain/cvs_stuff/schema/chado/lib';
+use lib '/home/scott/cvs_stuff/schema/chado/lib';
+use Bio::GMOD::Config;
+use Bio::GMOD::DB::Config;
+use ExtUtils::MakeMaker;  #to get prompt
+
+=head1 NAME
+
+$0 - updates the schema of a Chado database if necessary
+
+=head1 SYNOPSIS
+
+  % gmod_update_chado.pl [options] 
+
+=head1 COMMAND-LINE OPTIONS
+
+  --force          Update the schema without prompt
+  --dbprofile      Which database profile to use for updating
+
+=head1 DESCRIPTION
+
+=head1 AUTHOR
+
+Scott Cain E<lt>scain@cpan.orgE<gt>
+
+Copyright (c) 2011
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+my ($FORCE, $DBPROFILE, );
+
+GetOptions(
+    'force'         => \$FORCE,
+    'dbprofile=s'   => \$DBPROFILE,
+) or ( system( 'pod2text', $0 ), exit -1 );
+
+$DBPROFILE ||= 'default';
+
+my $gmod_conf = Bio::GMOD::Config->new();
+my $version   = $gmod_conf->version();
+my $gmod_root = $gmod_conf->gmod_root();
+my $db_conf   = Bio::GMOD::DB::Config->new($gmod_conf, $DBPROFILE);
+my $dbh       = $db_conf->dbh();
+
+my $current_version = `gmod_chado_properties.pl --version --dbprof $DBPROFILE`;
+chomp $current_version;
+
+if ($current_version >= $version) {
+    print "This instance of the Chado schema does not need updating.\n";
+    exit(0);
+}
+
+#build path to get updates from
+my $path = "$gmod_root/src/chado/schemas/$current_version-$version/diff.sql";
+warn $path;
+
+my $dbuser = $db_conf->user;
+my $dbport = $db_conf->port;
+my $dbhost = $db_conf->host;
+my $dbname = $db_conf->name;
+
+my $syscommand = "cat $path | psql -U $dbuser -p $dbport -h $dbhost $dbname";
+warn $syscommand;
+system($syscommand) == 0 or die "failed updating database";
+
+print "Updating $dbname complete.\n";
+
+exit(0);
+
