38bc9174f97bac4b7745d1e91dbaa3be5c3cf370
scottcain <scottcain@224a875b-6a50-0410-9993-82261b5d0d45>
2011-11-17 17:47:08 +0000

    created a script to automatically create schmema diff dirs

---
+++ schema/trunk/chado/schemas/generate_diff_dirs.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Scalar::Util qw(looks_like_number);
+use File::Copy;
+
+my $VERSION = $ARGV[0];
+
+die unless $VERSION;
+
+#make this versions directory
+mkdir $VERSION;
+copy("../modules/default_schema.sql", $VERSION);
+system("svn add $VERSION");
+
+#make the diff dirs and skelton diff files
+my @add_to_manifest;
+my @dirs = <*>;
+
+for my $dir (@dirs) {
+    next unless -d $dir;
+
+    if (looks_like_number($dir)) {
+        next if ($dir == $VERSION);
+        my $newdir = $dir.'-'.$VERSION;
+        mkdir $newdir;
+        system("touch $newdir/diff.sql");
+        push @add_to_manifest, "schemas/$newdir", "schemas/$newdir/diff.sql";
+        system("svn add $newdir");
+    }
+}
+
+#add the created files to the MANIFEST
+my $manifest = "../MANIFEST";
+open(MANIFEST, ">>", $manifest) or die;
+print MANIFEST "#the following added by generate_diff_dirs.pl\n";
+for (@add_to_manifest) {
+    print MANIFEST "$_\n";
+}
+close MANIFEST;
