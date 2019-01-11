a8ac3ada2252a439118216e905a0ff5565be4e6c
Sofia Robb <sofia2341@gmail.com>
2019-01-11 09:40:42 -0800

    removed TYPE

--- chado/migrations/V1.3.3.005__add_type_id_2_project_table.sql
+++ chado/migrations/V1.3.3.005__add_type_id_2_project_table.sql
@@ -1,6 +1,6 @@
 /* https://github.com/GMOD/Chado/issues/37 */
 ALTER TABLE project
-ADD COLUMN type_id TYPE bigint;
+ADD COLUMN type_id bigint;
 COMMENT ON COLUMN project.type_id IS 'An optional cvterm_id that specifies what type of project this record is.  Prior to 1.4, project type was set with an projectprop.';
 
 
