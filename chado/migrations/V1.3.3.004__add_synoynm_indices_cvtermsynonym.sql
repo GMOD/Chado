568623910f8158008f4e0f7432b06dc3de7cc9e3
bradfordcondon <bradford.condon@gmail.com>
2019-01-10 16:54:55 -0800

    fix formatting of migration

--- chado/migrations/V1.3.3.004__add_synoynm_indices_cvtermsynonym.sql
+++ chado/migrations/V1.3.3.004__add_synoynm_indices_cvtermsynonym.sql
@@ -1,3 +1,3 @@
 /* https://github.com/GMOD/Chado/issues/70 */
-CREATE INDEX ON cvtermsynonym type_id;
-CREATE INDEX ON cvtermsynonym synonym;
+CREATE INDEX cvtermsynonym_idx2 ON cvtermsynonym (type_id);
+CREATE INDEX cvtermsynonym_idx3 ON cvtermsynonym (synonym);
