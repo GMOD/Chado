7e3b03b5ef23065b1bfcbcfeecb05dfc1b668e1d
Ferrisx4 <ferrisx4@gmail.com>
2022-03-08 15:55:33 -0500

    Syntax fixes (remove quotation marks)

--- chado/migrations/V1.3.3.012_add_contact_dbxref_table.sql
+++ chado/migrations/V1.3.3.012_add_contact_dbxref_table.sql
@@ -5,11 +5,11 @@ CREATE TABLE contact_dbxref (
     is_current INT NOT NULL,
     PRIMARY KEY (contact_dbxref_id),
     FOREIGN KEY (dbxref_id) REFERENCES dbxref (dbxref_id) ON DELETE CASCADE INITIALLY DEFERRED,
-    CONSTRAINT "contact_dbxref_contact_id_fkey" FOREIGN KEY (contact_id) REFERENCES contact (contact_id) ON DELETE CASCADE INITIALLY DEFERRED,
+    CONSTRAINT contact_dbxref_contact_id_fkey FOREIGN KEY (contact_id) REFERENCES contact (contact_id) ON DELETE CASCADE INITIALLY DEFERRED,
 );
 
 create index contact_dbxref_idx1 on contact_dbxref (contact_id);
 create index contact_dbxref_idx2 on contact_dbxref (dbxref_id);
 
-COMMENT ON TABLE "chado"."contact_dbxref" IS 'A contact_dbxref is a dbxref associated with a contact.
-A contact can have many dbxrefs associated with it, and a dbxref can be associated with many contacts.';
+COMMENT ON TABLE contact_dbxref IS 'A contact_dbxref is a dbxref associated with a contact.
+A contact can have many dbxrefs associated with it, and a dbxref can be associated with many contacts.'
